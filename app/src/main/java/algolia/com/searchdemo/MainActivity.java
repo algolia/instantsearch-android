84c3858d8016efedb8a51d1d13e2117249598fdc
Paul-Louis NECH <pln@algolia.com>
2016-04-26 15:21:52 +0200

    MainActivity: Created Searchable Activity

--- app/src/main/java/algolia/com/searchdemo/MainActivity.java
+++ app/src/main/java/algolia/com/searchdemo/MainActivity.java
@@ -1,13 +1,39 @@
 package algolia.com.searchdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends ListActivity {
+
+    @BindView(android.R.id.list)
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setListAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_selectable_list_item,
+                getResources().getStringArray(R.array.data)));
+
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            search(query);
+        }
+    }
+
+    private void search(String query) {
+        throw new IllegalStateException("Not implemented!");
     }
 }
