ad77b84a1a950cf955f40caae3b988a1b9ebc2d5
Paul-Louis NECH <pln@algolia.com>
2016-04-27 12:03:30 +0200

    Search: searching through local data

--- app/src/main/java/algolia/com/searchdemo/MainActivity.java
+++ app/src/main/java/algolia/com/searchdemo/MainActivity.java
@@ -2,29 +2,42 @@ package algolia.com.searchdemo;
 
 import android.app.ListActivity;
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 
+import java.util.ArrayList;
+
+import butterknife.BindArray;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends ListActivity {
 
     @BindView(android.R.id.list)
-    ListView list;
+    ListView listView;
+
+    @BindView(R.id.searchBox)
+    SearchView searchBox;
+
+    @BindArray(R.array.data)
+    String[] dataList;
+
+    private int itemStyleResourceId = android.R.layout.simple_list_item_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        setListAdapter(new ArrayAdapter<>(this,
-                android.R.layout.simple_selectable_list_item,
-                getResources().getStringArray(R.array.data)));
 
+        setListAdapter(new ArrayAdapter<>(this, itemStyleResourceId, dataList));
+        SearchManager manager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchBox.setSearchableInfo(manager.getSearchableInfo(getComponentName()));
+        searchBox.setIconifiedByDefault(false);
 
         Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
@@ -34,6 +47,27 @@ public class MainActivity extends ListActivity {
     }
 
     private void search(String query) {
-        throw new IllegalStateException("Not implemented!");
+        ArrayList<String> matchingList = new ArrayList<>();
+
+        for (String item : dataList) {
+            if (levenshtein(item, query) < 2) {
+                matchingList.add(item);
+            }
+        }
+        getListView().setAdapter(new ArrayAdapter<>(this, itemStyleResourceId, matchingList));
+    }
+
+    private int levenshtein(String a, String b) {
+        if (a == null && b == null) return 0;
+        if (a == null || a.length() == 0 ) return b.length();
+        if (b == null || b.length() == 0 ) return a.length();
+
+        if (a.charAt(0) == b.charAt(0)) return levenshtein(a.substring(1), b.substring(1));
+
+        int distanceA = levenshtein(a, b.substring(1));
+        int distanceB = levenshtein(a.substring(1), b.substring(1));
+        int distanceC = levenshtein(a.substring(1), b);
+
+        return 1 + Math.min(distanceA, Math.min(distanceB, distanceC));
     }
 }
