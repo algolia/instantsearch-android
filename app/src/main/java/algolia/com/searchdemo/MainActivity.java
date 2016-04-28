25879e93b1ffcdc5750d3fdf719097dc45b6811c
Paul-Louis NECH <pln@algolia.com>
2016-04-28 12:17:43 +0200

    Search: Using algolia aon each keystroke

--- app/src/main/java/algolia/com/searchdemo/MainActivity.java
+++ app/src/main/java/algolia/com/searchdemo/MainActivity.java
@@ -5,39 +5,70 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.algolia.search.saas.AlgoliaException;
+import com.algolia.search.saas.Index;
+import com.algolia.search.saas.Query;
+import com.algolia.search.saas.listeners.SearchListener;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import algolia.com.searchdemo.helper.AlgoliaHelper;
 import butterknife.BindArray;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends ListActivity {
 
-    @BindView(android.R.id.list)
-    ListView listView;
-
     @BindView(R.id.searchBox)
-    SearchView searchBox;
+    SearchBox searchBox;
 
     @BindArray(R.array.data)
     String[] dataList;
 
-    private int itemStyleResourceId = android.R.layout.simple_list_item_1;
+    private ArrayAdapter<String> adapter;
+    private AlgoliaHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
         resetList();
+        helper = new AlgoliaHelper("9V0VSE2N4Z", "7c4db7c9e62611b1eb078a220044f546", "contacts");
+        helper.setActivity(this);
+
         SearchManager manager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         searchBox.setSearchableInfo(manager.getSearchableInfo(getComponentName()));
         searchBox.setIconifiedByDefault(false);
+        searchBox.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.e("PLN", "Submit!");
+                Toast.makeText(MainActivity.this, "Submit!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.e("PLN", "Change!");
+                Toast.makeText(MainActivity.this, "Change!", Toast.LENGTH_SHORT).show();
+                if (newText.length() == 0) {
+                    resetList();
+                } else {
+                    search(newText);
+                }
+                return true;
+            }
+        });
 
         handleIntent(getIntent());
     }
@@ -56,18 +87,46 @@ public class MainActivity extends ListActivity {
     }
 
     private void search(String query) {
-        ArrayList<String> matchingList = new ArrayList<>();
+        helper.search(query, new SearchListener() {
+                    @Override
+                    public void searchResult(Index index, Query query, JSONObject jsonResults) {
+                        List<String> results = helper.parseResults(jsonResults);
+                        adapter.clear();
+                        adapter.addAll(results);
+                        adapter.notifyDataSetChanged();
 
-        for (String item : dataList) {
-            if (levenshtein(item, query) < 2) {
-                matchingList.add(item);
-            }
-        }
-        getListView().setAdapter(new ArrayAdapter<>(this, itemStyleResourceId, matchingList));
+                        getListView().smoothScrollToPosition(0);
+                    }
+
+                    @Override
+                    public void searchError(Index index, Query query, AlgoliaException e) {
+                        Toast.makeText(MainActivity.this, "Error:" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+        );
+//
+//        ArrayList<String> matchingList = new ArrayList<>();
+//
+//        for (String item : dataList) {
+//            if (levenshtein(item, query) < 2) {
+//                matchingList.add(item);
+//            }
+//        }
+//
+//        getListView().setAdapter(new ArrayAdapter<>(this, itemStyleResourceId, matchingList));
     }
 
+
     private void resetList() {
-        setListAdapter(new ArrayAdapter<>(this, itemStyleResourceId, dataList));
+        if (adapter == null) {
+            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<>(Arrays.asList(dataList)));
+            setListAdapter(adapter);
+        } else {
+            adapter.clear();
+            adapter.addAll(dataList);
+            adapter.notifyDataSetChanged();
+        }
     }
 
     private int levenshtein(String a, String b) {
---
+++ app/src/main/java/algolia/com/searchdemo/helper/AlgoliaHelper.java
@@ -0,0 +1,92 @@
+package algolia.com.searchdemo.helper;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+
+import com.algolia.search.saas.APIClient;
+import com.algolia.search.saas.AlgoliaException;
+import com.algolia.search.saas.Index;
+import com.algolia.search.saas.Query;
+import com.algolia.search.saas.listeners.SearchListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlgoliaHelper {
+    private static final int HITS_PER_PAGE = 20;
+
+    private final Index index;
+    private final APIClient client;
+    private final Query query;
+
+    public AlgoliaHelper(final String applicationId, final String apiKey, final String indexName) {
+        client = new APIClient(applicationId, apiKey);
+        index = client.initIndex(indexName);
+        query = new Query();
+        query.setAttributesToRetrieve("name", "company", "country", "followers");
+        query.setAttributesToHighlight("name", "company");
+        query.setHitsPerPage(HITS_PER_PAGE);
+    }
+
+
+    public void search(final String queryString, final SearchListener listener) {
+        SearchListener logListener = new SearchListener() {
+            @Override
+            public void searchResult(Index index, Query query, JSONObject results) {
+                Log.d("PLN|search.searchResult", String.format("Index %s with query %s succeeded: %s.", index.getIndexName(), queryString, results));
+                listener.searchResult(index, query, results);
+            }
+
+            @Override
+            public void searchError(Index index, Query query, AlgoliaException e) {
+                Log.d("PLN|search.searchError", String.format("Index %s with query %s failed: %s(%s).", index.getIndexName(), queryString, e.getCause(), e.getMessage()));
+                listener.searchError(index, query, e);
+            }
+        };
+        query.setQueryString(queryString);
+        index.searchASync(query, logListener);
+    }
+
+    public List<String> parseResults(final JSONObject jsonObject) {
+        if (jsonObject == null) {
+            return null;
+        }
+
+        List<String> results = new ArrayList<>();
+        JSONArray hits = jsonObject.optJSONArray("hits");
+        if (hits == null) {
+            return null;
+        }
+
+        for (int i = 0; i < hits.length(); ++i) {
+            JSONObject hit = hits.optJSONObject(i);
+            if (hit == null) {
+                continue;
+            }
+
+            JSONObject highlightResult = hit.optJSONObject("_highlightResult");
+            if (highlightResult == null) {
+                continue;
+            }
+            String name;
+            try {
+                name = highlightResult.getString("name");
+            } catch (JSONException e) {
+                continue;
+            }
+            if (name == null) {
+                continue;
+            }
+
+            results.add(name);
+        }
+        return results;
+    }
+}
