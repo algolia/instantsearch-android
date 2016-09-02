ea6ac95806c609998868e9e36bf2494268df917c
Paul-Louis NECH <pln@algolia.com>
2016-09-02 15:20:11 +0200

    RefinementList: Stop logging on error

--- instantsearch/src/main/java/com/algolia/instantsearch/views/RefinementList.java
+++ instantsearch/src/main/java/com/algolia/instantsearch/views/RefinementList.java
@@ -7,7 +7,6 @@ import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -151,7 +150,6 @@ public class RefinementList extends ListView implements AlgoliaWidget {
     }
 
     @Override public void onError(Query query, @NonNull AlgoliaException error) {
-        Log.e("InstantSearch", String.format(Errors.REFINEMENTS_RECEIVED_ERROR, error.getMessage())); //DISCUSS: Should we log errors or rethrow?
     }
 
     @Override public void onReset() {
