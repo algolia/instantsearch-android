bd38b6cfd230aa2857a49273e969e8ef6c06f147
Paul-Louis NECH <pln@algolia.com>
2016-09-14 13:41:01 +0200

    OnDemandStrategy: add triggerSearch() and enableAndSearch() methods

--- instantsearch/src/main/java/com/algolia/instantsearch/strategies/OnDemandStrategy.java
+++ instantsearch/src/main/java/com/algolia/instantsearch/strategies/OnDemandStrategy.java
@@ -1,10 +1,17 @@
 package com.algolia.instantsearch.strategies;
 
+import com.algolia.instantsearch.Searcher;
+
 /**
  * This {@link SearchStrategy} only fires requests when enabled.
  */
 public class OnDemandStrategy implements SearchStrategy {
+    private final Searcher searcher;
     private boolean enabled = false;
+
+    public OnDemandStrategy(Searcher searcher) {
+        this.searcher = searcher;
+    }
 
     @Override
     public boolean beforeSearch(Searcher searcher, String queryString) {
@@ -16,12 +23,33 @@ public class OnDemandStrategy implements SearchStrategy {
         return false;
     }
 
+    /**
+     * Triggers a search request with searcher's current state.
+     */
+    public void triggerSearch() {
+        searcher.search();
+    }
+
+    /**
+     * Enables this strategy, future requests will be fired.
+     */
     public void enable() {
         this.enabled = true;
     }
 
+    /**
+     * Disables this strategy, future requests will be blocked.
+     */
     public void disable() {
         this.enabled = false;
+    }
+
+    /**
+     * Enables this strategy and triggers a search request.
+     */
+    public void enableAndSearch() {
+        enable();
+        triggerSearch();
     }
 
     public boolean isEnabled() {
