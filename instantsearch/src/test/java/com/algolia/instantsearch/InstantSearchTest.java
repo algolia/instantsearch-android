6f5657b35ba20ab21f68c04d3b38e72a219eda30
Paul-Louis NECH <pln@algolia.com>
2016-11-23 18:03:40 +0100

    fix(InstantSearchTest): Correct manifest location

--- instantsearch/src/test/java/com/algolia/instantsearch/InstantSearchTest.java
+++ instantsearch/src/test/java/com/algolia/instantsearch/InstantSearchTest.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-@Config(manifest = Config.NONE, constants = BuildConfig.class)
+@Config(manifest = "src/main/AndroidManifest.xml")
 @RunWith(RobolectricTestRunner.class)
 public abstract class InstantSearchTest {
 }
