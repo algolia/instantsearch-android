b54e8f97705e1020fa400fbb39ee87febc22fad1
Paul-Louis NECH <pln@algolia.com>
2016-12-16 11:43:28 +0100

    fix(Errors): Mention every sortBy possible values

--- instantsearch/src/main/java/com/algolia/instantsearch/model/Errors.java
+++ instantsearch/src/main/java/com/algolia/instantsearch/model/Errors.java
@@ -20,7 +20,7 @@ public class Errors {
     public static final String REFINEMENTS_MISSING_ATTRIBUTE = "You need to specify the attribute to refine on with algolia:attribute.";
     public static final String REFINEMENTS_MISSING_ITEM = "RefinementList has no item at position %d.";
 
-    public static final String SORT_INVALID_VALUE = "invalid sortBy value: %s is none of \"count\"|\"isRefined\"|\"name:asc\"|\"name:desc\".";
+    public static final String SORT_INVALID_VALUE = "invalid sortBy value: %s is none of \"isRefined\"|\"count:asc\"|\"count:desc\"|\"name:asc\"|\"name:desc\".";
     public static final String SORT_INVALID_ARRAY = "invalid sortBy value: %s is not a valid JSONArray.";
 
     public static final String PROGRESS_WITHOUT_SEARCHPLATE = "Could not find a view with id @id/search_plate in SearchView.";
