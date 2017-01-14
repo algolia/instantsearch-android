6fc241bf06ee1477e62c6cc01317fb8d3b1f8ed5
Paul-Louis NECH <pln@algolia.com>
2017-01-14 19:09:58 +0100

    docs: Update root path

--- docgen/config.js
+++ docgen/config.js
@@ -5,7 +5,7 @@ const prod = process.env.NODE_ENV === 'production';
 export default {
   docsDist: prod ?
     rootPath('docs-production/react') : // avoids publishing an `npm start`ed build if running.
-    rootPath('docs/react'),
+    rootPath('docs/site'),
   publicPath: prod ?
     'https://community.algolia.com/instantsearch-android/' :
     '/',
