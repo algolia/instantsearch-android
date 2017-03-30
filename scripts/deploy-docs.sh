e05dbe9b88ca559e110128966df9c0ccec285ec2
Paul-Louis NECH <pln@algolia.com>
2017-03-30 11:38:06 +0200

    refact(scripts): simplify scripts

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 set -e # exit when error
-./scripts/validate-status.sh
 
-printf "\n\nRelease: update working tree"
+currentBranch=`git rev-parse --abbrev-ref HEAD`
+if [ $currentBranch != 'master' ]; then
+  printf "Release: You must be on maste§r\n"
+  exit 1
+fi
+
+if [[ -n $(git status --porcelain | grep -v '??') ]]; then
+  printf "Release: Working tree is not clean (git status)\n"
+  exit 1
+fi
+
+printf "\n\nRelease: update working tree\n"
 git pull origin master
 git fetch origin --tags
 
+printf "\n\nRelease: generate and copy javadoc\n"
+rm -rf docgen/src/javadoc/
+./gradlew copyJavadoc
+git add docgen/src/javadoc/
+
+printf "\n\nRelease: build static documentation\n"
 (cd docgen && NODE_ENV=production npm run build)
 git add docs/
 git commit -m "chore: Update docs"
