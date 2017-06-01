333f74ef5cdcaf569225e75eff035fbc199d1c19
Paul-Louis NECH <pln@algolia.com>
2017-06-01 12:26:57 -0700

    chore(Stats): Remove obsolete todo

--- instantsearch/src/main/java/com/algolia/instantsearch/ui/views/Stats.java
+++ instantsearch/src/main/java/com/algolia/instantsearch/ui/views/Stats.java
@@ -13,7 +13,6 @@ import com.algolia.search.saas.Query;
 
 
 public class Stats extends android.support.v7.widget.AppCompatTextView implements AlgoliaWidget {
-    // TODO: Autohidecontainer, maybe other useful attrs? https://community.algolia.com/instantsearch.js/documentation/#stats
     /** The default template, only shown when there is no error. */
     public static final String DEFAULT_TEMPLATE = "{nbHits} results found in {processingTimeMS} ms";
 
