6a23e4f8f5a153bc7397645b790f49e87558e7ef
Paul-Louis NECH <pln@algolia.com>
2017-07-19 18:54:11 +0200

    refact(Toggle): extend Switch instead of CheckBox

--- instantsearch/src/main/java/com/algolia/instantsearch/ui/views/filters/Toggle.java
+++ instantsearch/src/main/java/com/algolia/instantsearch/ui/views/filters/Toggle.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.SwitchCompat;
 import android.util.AttributeSet;
 
 import com.algolia.instantsearch.R;
@@ -21,7 +21,7 @@ import org.greenrobot.eventbus.EventBus;
 /**
  * A widget that toggles between refining and not refining an attribute with a given value.
  */
-public abstract class Toggle extends AppCompatCheckBox implements AlgoliaFacetFilter, AlgoliaResultListener, AlgoliaErrorListener, AlgoliaSearcherListener {
+public abstract class Toggle extends SwitchCompat implements AlgoliaFacetFilter, AlgoliaResultListener, AlgoliaErrorListener, AlgoliaSearcherListener {
     private final EventBus bus;
     /** The attribute to refine on. */
     public String attribute;
