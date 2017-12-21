f84bf2f7d2097a3cb9e1143817003e89c426a9f2
Paul-Louis NECH <pln@algolia.com>
2017-12-21 16:22:28 +0100

    refactor(RenderingHelper): Store highlights using view/attribute pairs

--- instantsearch/src/main/java/com/algolia/instantsearch/ui/databinding/RenderingHelper.java
+++ instantsearch/src/main/java/com/algolia/instantsearch/ui/databinding/RenderingHelper.java
@@ -2,6 +2,10 @@ package com.algolia.instantsearch.ui.databinding;
 
 import android.support.annotation.ColorRes;
 import android.support.annotation.NonNull;
+import android.util.Pair;
+import android.view.View;
+
+import com.algolia.instantsearch.R;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -9,21 +13,19 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * Contains mappings between an attribute to highlight and its appropriate highlighting color.
+ * Contains mappings between a view+attribute to highlight and its appropriate highlighting color.
  */
 @SuppressWarnings("UnusedReturnValue")
 public class RenderingHelper {
-    final static String DEFAULT_COLOR = "@color/colorHighlighting";
+    final static int DEFAULT_COLOR = R.color.colorHighlighting;
 
     /** The default RenderingHelper, highlighting with the {@link RenderingHelper#DEFAULT_COLOR default color}. */
     private static RenderingHelper defaultRenderingHelper;
 
-    /** The Set of attributes to highlight. */
-    @NonNull
-    private final Set<String> highlightedAttributes;
-    /** A Map associating attributes with their respective highlighting {@link ColorRes color}. */
-    @NonNull
-    private final Map<String, Integer> attributeColors;
+    /** The Set of view/attribute pairs to highlight. */
+    @NonNull private final Set<Pair<View, String>> highlightedAttributes;
+    /** A Map associating view/attribute pairs with their respective highlighting {@link ColorRes color}. */
+    @NonNull private final Map<Pair<View, String>, Integer> attributeColors;
 
     /**
      * Gets the {@link RenderingHelper#defaultRenderingHelper default RenderingHelper}.
@@ -43,47 +45,41 @@ public class RenderingHelper {
     }
 
     /**
-     * Gets the highlighting color for a given attribute.
+     * Gets the highlighting color for a given view/attribute pair.
      *
+     * @param view      the view using this attribute.
      * @param attribute the attribute's name.
-     * @return the {@link ColorRes} associated with this attribute, or 0 if there is none.
+     * @return the {@link ColorRes} associated with this view/attribute pair, or 0 if there is none.
      */
-    public @ColorRes Integer getHighlightColor(String attribute) {
+    public @ColorRes Integer getHighlightColor(View view, String attribute) {
         try {
-            return attributeColors.get(attribute);
+            return attributeColors.get(new Pair<>(view, attribute));
         } catch (NullPointerException e) {
             return 0;
         }
     }
 
     /**
-     * Checks if an attribute should be highlighted.
+     * Checks if an attribute should be highlighted in a view.
      *
+     * @param view      the view using this attribute.
      * @param attribute the attribute's name.
      * @return {@code true} if the attribute was marked for highlighting.
      */
-    public boolean shouldHighlight(String attribute) {
-        return highlightedAttributes.contains(attribute);
+    public boolean shouldHighlight(View view, String attribute) {
+        return highlightedAttributes.contains(new Pair<>(view, attribute));
     }
 
     /**
-     * Sets a color for this attribute's highlighting.
+     * Enables highlighting for this view/attribute pair.
      *
      * @param attribute the attribute to color.
      * @param colorId   a {@link ColorRes} to associate with this attribute.
      * @return the previous color associated with this attribute or {@code null} if there was none.
      */
-    Integer addColor(String attribute, @ColorRes int colorId) {
-        return attributeColors.put(attribute, colorId);
-    }
-
-    /**
-     * Enables highlighting for this attribute.
-     *
-     * @param attribute the attribute to color.
-     * @return {@code true} if the attribute was not already highlighted, {@code false} otherwise.
-     */
-    boolean addHighlight(String attribute) {
-        return highlightedAttributes.add(attribute);
+    Integer addHighlight(View view, String attribute, @ColorRes int colorId) {
+        final Pair<View, String> pair = new Pair<>(view, attribute);
+        highlightedAttributes.add(pair);
+        return attributeColors.put(pair, colorId);
     }
 }
