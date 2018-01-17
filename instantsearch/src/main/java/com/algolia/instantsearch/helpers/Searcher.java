3acbe49ab1572ce39fb4858d5b6ac92fb3b3636f
Paul-Louis NECH <pln@algolia.com>
2018-01-17 18:47:39 +0100

    fix(Searcher): Restore missing import

--- instantsearch/src/main/java/com/algolia/instantsearch/helpers/Searcher.java
+++ instantsearch/src/main/java/com/algolia/instantsearch/helpers/Searcher.java
@@ -37,6 +37,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
