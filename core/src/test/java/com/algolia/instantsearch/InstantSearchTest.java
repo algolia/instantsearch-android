2a7f8c1dc0a42a6f5038a5d4ad9153c505afd995
Paul-Louis NECH <pln@algolia.com>
2018-10-30 14:38:41 +0100

    refactor: Inline gradle vars, update robolectric to 4.0

--- core/src/test/java/com/algolia/instantsearch/InstantSearchTest.java
+++ core/src/test/java/com/algolia/instantsearch/InstantSearchTest.java
@@ -1,12 +1,8 @@
 package com.algolia.instantsearch;
 
-import com.algolia.instantsearch.core.BuildConfig;
-
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
 
 @RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class)
 public abstract class InstantSearchTest {
 }
