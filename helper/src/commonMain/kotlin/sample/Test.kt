e5307c152501df79e6b9b5fed4e3b451b553161c
Quentin LITZLER <qlitzler@gmail.com>
2019-01-08 15:01:26 +0100

    feat: Initial commit

---
+++ helper/src/commonMain/kotlin/sample/Test.kt
@@ -0,0 +1,5 @@
+package sample
+
+fun test() {
+    Sample()
+}
---
+++ helper/src/jvmTest/kotlin/sample/TestSomething.kt
@@ -0,0 +1,12 @@
+package sample
+
+import org.junit.Test
+
+class TestSomething {
+
+    @Test
+    fun stuff() {
+        Sample()
+        test()
+    }
+}
