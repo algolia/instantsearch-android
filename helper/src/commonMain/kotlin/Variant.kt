149e8df8f3a091d340b97e5942a3921db11f2834
Quentin LITZLER <qlitzler@gmail.com>
2019-03-11 13:05:27 +0100

    Reformat Variant class

--- helper/src/commonMain/kotlin/Variant.kt
+++ helper/src/commonMain/kotlin/Variant.kt
@@ -2,10 +2,14 @@ package model
 
 import com.algolia.search.exception.EmptyStringException
 
+
 data class Variant(val name: String) {
+
     init {
         if (name.isEmpty()) throw EmptyStringException("Variant")
     }
 
-    override fun toString(): String = name
+    override fun toString(): String {
+        return name
+    }
 }
