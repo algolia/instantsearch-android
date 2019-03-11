12b6b57c32270c07506564af1bba53cc309cacac
Paul-Louis NECH <pln@algolia.com>
2019-03-11 16:23:02 +0100

    feat(HierarchicalMenu): ViewModel

--- core/src/commonTest/kotlin/Test.kt
+++ core/src/commonTest/kotlin/Test.kt
@@ -1,6 +1,33 @@
+
 import kotlinx.coroutines.CoroutineScope
-import kotlin.reflect.KClass
-import kotlin.test.*
+import searcher.Searcher
 
 
 internal expect fun blocking(block: suspend CoroutineScope.() -> Unit)
+
+internal data class Facet(
+    val attribute: String,
+    val count: Int
+)
+
+internal val facets = listOf(
+    Facet("Blue", 1),
+    Facet("Red", 2),
+    Facet("Green", 3)
+)
+internal val otherFacets = listOf(
+    Facet("Blue", 1),
+    Facet("Red", 2)
+)
+
+internal class MockSearcher : Searcher {
+
+    var count = 0
+
+    override fun search() {
+        count++
+    }
+
+    override fun cancel() {
+    }
+}
