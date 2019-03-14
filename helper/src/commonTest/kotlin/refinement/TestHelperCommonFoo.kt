9291205a627cff4803ac914ab9bde763c835850f
Paul-Louis NECH <pln@algolia.com>
2019-03-14 14:04:56 +0100

    chore: Remove duplicate JVM

---
+++ helper/src/commonTest/kotlin/refinement/TestHelperCommonFoo.kt
@@ -0,0 +1,10 @@
+package refinement
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TestFoo {
+    @Test fun `foo should be bar`(){
+        assertEquals("foo", "bar")
+    }
+}
