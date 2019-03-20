27ac377ea14f486c0437cd3bcc4013ef39b8546e
Paul-Louis NECH <pln@algolia.com>
2019-03-20 15:36:21 +0100

    refactor: Migrate to androidX

--- sample/src/main/java/com/algolia/instantsearch/android/sample/MainActivity.kt
+++ sample/src/main/java/com/algolia/instantsearch/android/sample/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.algolia.instantsearch.android.sample
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
