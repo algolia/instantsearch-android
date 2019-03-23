5f46535f0de639a32b2b3fb46104d00aeed9406c
Quentin LITZLER <qlitzler@gmail.com>
2019-03-23 14:16:37 +0100

    Update gradle

---
+++ helper/src/androidTest/kotlin/Actual.kt
@@ -0,0 +1,12 @@
+import com.algolia.search.model.APIKey
+import com.algolia.search.model.ApplicationID
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.runBlocking
+
+
+internal actual val apiKey = APIKey(System.getenv("ALGOLIA_KOTLIN_CLIENT_API_KEY")!!)
+internal actual val applicationID = ApplicationID(System.getenv("ALGOLIA_KOTLIN_CLIENT_APP_ID")!!)
+
+internal actual fun blocking(block: suspend CoroutineScope.() -> Unit) {
+    runBlocking(block = block)
+}
