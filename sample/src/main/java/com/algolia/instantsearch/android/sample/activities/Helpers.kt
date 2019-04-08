ceed2d60fc2f7e11d9a30e977e00c64a0caa140c
Paul-Louis NECH <pln@algolia.com>
2019-04-08 11:37:33 +0200

    refactor(ToggleActivity): Extract helpers

--- sample/src/main/java/com/algolia/instantsearch/android/sample/activities/Helpers.kt
+++ sample/src/main/java/com/algolia/instantsearch/android/sample/activities/Helpers.kt
@@ -2,11 +2,18 @@ package com.algolia.instantsearch.android.sample.activities
 
 import android.content.Context
 import android.widget.Toast
+import com.algolia.instantsearch.android.Stats
+import com.algolia.instantsearch.android.sample.R.id.checkedTextView
+import com.algolia.instantsearch.android.sample.R.id.statsView
+import com.algolia.instantsearch.android.sample.views.OneValueToggle
 import com.algolia.search.client.ClientSearch
 import com.algolia.search.model.APIKey
 import com.algolia.search.model.ApplicationID
+import com.algolia.search.model.Attribute
 import com.algolia.search.model.IndexName
+import com.algolia.search.model.search.Facet
 import com.algolia.search.model.search.Query
+import refinement.*
 import searcher.SearcherSingleIndex
 
 
@@ -19,4 +26,34 @@ internal fun initSearcherSingleIndex(): SearcherSingleIndex {
     )
 }
 
+internal fun Context.addDebugListeners(searcher: SearcherSingleIndex) {
+    searcher.responseListeners += {
+        toast("Response")
+    }
+    searcher.errorListeners += {
+        toast("Error: $it")
+    }
+}
+
+internal fun prepareWidgets(searcher: SearcherSingleIndex, widgets: List<Any>) {
+    val state = SearchFilterState()
+
+    widgets.forEach {
+        if (it is Stats) {
+            it.connectWithSearcher(searcher)
+        } else if (it is OneValueToggle) {
+            // Links view <> RefinementModel listeners, to keep display in sync with model
+            val model = RefinementListViewModel<Facet>(RefinementListViewModel.Mode.SingleChoice) // Sets selection mode for presentation
+            model.connectView(it)
+
+            // Links model to state, updating state on refinement and presenting on new state
+            state.connectRefinementModel(model, it.attribute, Operator.AND)
+
+            // Links model to searcher, updating model -> view on new results
+            model.connectSearcherSingleIndex(searcher, it.attribute)
+            state.connectSearcherSingleIndex(searcher) // Links state to searcher, querying on new state
+        }
+    }
+
+}
 fun Context.toast(text: String, length: Int = Toast.LENGTH_SHORT) = Toast.makeText(this, text, length).show()
--- sample/src/main/java/com/algolia/instantsearch/android/sample/activities/ToggleActivity.kt
+++ sample/src/main/java/com/algolia/instantsearch/android/sample/activities/ToggleActivity.kt
@@ -6,41 +6,21 @@ import com.algolia.instantsearch.android.Stats
 import com.algolia.instantsearch.android.sample.R
 import com.algolia.instantsearch.android.sample.views.OneValueToggle
 import com.algolia.search.model.Attribute
-import com.algolia.search.model.search.Facet
 import kotlinx.android.synthetic.main.activity_toggle.*
-import refinement.*
-import refinement.RefinementListViewModel.Mode.SingleChoice
 
 class ToggleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_toggle)
+
         val searcher = initSearcherSingleIndex()
-        searcher.responseListeners += {
-            toast("Response!")
-        }
-        searcher.errorListeners += {
-            toast("Error: $it")
-        }
-        // Prepare widgets
-        val toggle = OneValueToggle(checkedTextView, Attribute("brand"), "Chanel")
-        val stats = Stats(statsView)
-        stats.connectWithSearcher(searcher)
+        addDebugListeners(searcher)
 
-        val state = SearchFilterState()
-        val model = RefinementListViewModel<Facet>(SingleChoice) // Sets selection mode for presentation
-        // Links view <> RefinementModel listeners, to keep display in sync with model
-        model.connectView(toggle)
+        prepareWidgets(
+            searcher, listOf(OneValueToggle(checkedTextView, Attribute("brand"), "Chanel"), Stats(statsView))
+        )
 
-        // Links model to state, updating state on refinement and presenting on new state
-        state.connectRefinementModel(model, toggle.attribute, Operator.AND)
-
-        // Links model to searcher, updating model -> view on new results
-        model.connectSearcherSingleIndex(searcher, toggle.attribute)
-        state.connectSearcherSingleIndex(searcher) // Links state to searcher, querying on new state
-
-        // Trigger search
         searcher.search()
     }
 }
