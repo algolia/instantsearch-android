4f5ba32ec9848fc09955081afebffb1378e6ed36
Quentin LITZLER <qlitzler@gmail.com>
2019-04-13 21:56:24 +0200

    Clean test

--- helper/src/androidTest/kotlin/Actual.kt
+++ helper/src/androidTest/kotlin/Actual.kt
@@ -1,11 +1,6 @@
-import com.algolia.search.model.APIKey
-import com.algolia.search.model.ApplicationID
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.runBlocking
 
-
-actual val apiKey = APIKey(System.getenv("ALGOLIA_KOTLIN_CLIENT_API_KEY")!!)
-actual val applicationID = ApplicationID(System.getenv("ALGOLIA_KOTLIN_CLIENT_APP_ID")!!)
 
 actual fun blocking(block: suspend CoroutineScope.() -> Unit) {
     runBlocking(block = block)
--- helper/src/commonTest/kotlin/Expect.kt
+++ helper/src/commonTest/kotlin/Expect.kt
@@ -7,11 +7,4 @@ import io.ktor.client.features.logging.LogLevel
 import kotlinx.coroutines.CoroutineScope
 
 
-internal expect val apiKey : APIKey
-internal expect val applicationID : ApplicationID
-
 internal expect fun blocking(block: suspend CoroutineScope.() -> Unit)
-
-internal val algolia get() = ClientSearch(ConfigurationSearch(applicationID, apiKey, logLevel = LogLevel.ALL))
-internal val indexA get() = algolia.initIndex(IndexName("products_android_demo"))
-internal val indexB get() = algolia.initIndex(IndexName("products_android_demo_name_asc"))
