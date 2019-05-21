e80e0370cfb953534083d120622e4ec6b5bb36ba
Paul-Louis NECH <pln@algolia.com>
2019-05-21 17:38:12 +0200

    feat(SearchBox): implement for EditText views

--- helper/src/androidMain/kotlin/com/algolia/instantsearch/helper/android/searchbox/SearchBox.kt
+++ helper/src/androidMain/kotlin/com/algolia/instantsearch/helper/android/searchbox/SearchBox.kt
@@ -78,4 +78,38 @@ sealed class SearchBox(internal val view: View) : SearchBoxView {
             }
         }
     }
+
+    abstract class EditText(editText: View) : SearchBox(editText) {
+
+        internal val textWatcher = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                changeListener?.invoke(s.toString())
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+        }
+
+        class System(private val editText: android.widget.EditText) : EditText(editText) {
+
+            override fun setTextChangeListener(listener: (String?) -> Unit) {
+                editText.removeTextChangedListener(textWatcher)
+                changeListener = listener
+                editText.addTextChangedListener(textWatcher)
+            }
+
+            override fun setTextSubmitListener(listener: (String?) -> Unit) = Unit
+        }
+
+        class Support(private val editText: androidx.appcompat.widget.AppCompatEditText) : EditText(editText) {
+
+            override fun setTextChangeListener(listener: (String?) -> Unit) {
+                editText.removeTextChangedListener(textWatcher)
+                changeListener = listener
+                editText.addTextChangedListener(textWatcher)
+            }
+
+            override fun setTextSubmitListener(listener: (String?) -> Unit) = Unit
+        }
+    }
 }
