1b668dc17c0123dd06a34005f6233fe0c6371018
Paul-Louis NECH <pln@algolia.com>
2019-05-22 18:03:09 +0200

    refactor(Demos): factorize stub Index

--- demo/src/main/kotlin/com/algolia/instantsearch/demo/searchbox/SearchOnSubmitDemo.kt
+++ demo/src/main/kotlin/com/algolia/instantsearch/demo/searchbox/SearchOnSubmitDemo.kt
@@ -1,24 +1,27 @@
 package com.algolia.instantsearch.demo.searchbox
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.algolia.instantsearch.core.searchbox.SearchBoxViewModel
-import com.algolia.instantsearch.demo.DemoActivity
-import com.algolia.instantsearch.demo.R
-import com.algolia.instantsearch.demo.configureRecyclerView
-import com.algolia.instantsearch.demo.configureSearchView
+import com.algolia.instantsearch.demo.*
 import com.algolia.instantsearch.demo.list.Movie
 import com.algolia.instantsearch.demo.list.MovieAdapter
 import com.algolia.instantsearch.helper.android.searchbox.connectView
-import com.algolia.instantsearch.helper.android.searcher.connectSearchBoxViewModel
+import com.algolia.instantsearch.helper.searchbox.connectSearcher
+import com.algolia.instantsearch.helper.searcher.SearcherSingleIndex
 import com.algolia.search.helper.deserialize
 import kotlinx.android.synthetic.main.demo_paging.*
 
-class SearchOnSubmitDemo : DemoActivity() {
+
+class SearchOnSubmitDemo : AppCompatActivity() {
+    private val searcher = SearcherSingleIndex(stubIndex)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.demo_search_on_submit)
         setSupportActionBar(toolbar)
 
+        searcher.index = client.initIndex(intent.indexName)
 
         val searchBoxViewModel = SearchBoxViewModel()
         searchBoxViewModel.connectView(searchView)
