781182ae22105ce5ab2d53a06474ca0577225b9d
Paul-Louis NECH <pln@algolia.com>
2019-05-27 12:01:32 +0200

    refactor: clearer mocks, use VMs in TestClearFilter

--- helper/src/androidTest/kotlin/filter/clear/TestClearFilter.kt
+++ helper/src/androidTest/kotlin/filter/clear/TestClearFilter.kt
@@ -1,19 +1,20 @@
 package filter.clear
 
-import android.view.View
-import android.widget.SearchView
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
-import com.algolia.instantsearch.helper.filter.clear.ClearFilterView
+import clearFilterView
+import com.algolia.instantsearch.core.searchbox.SearchBoxViewModel
+import com.algolia.instantsearch.helper.android.searchbox.connectSearchView
 import com.algolia.instantsearch.helper.filter.clear.ClearFilterViewModel
 import com.algolia.instantsearch.helper.filter.clear.connectState
 import com.algolia.instantsearch.helper.filter.clear.connectView
+import com.algolia.instantsearch.helper.searchbox.connectSearcher
 import com.algolia.instantsearch.helper.searcher.SearcherSingleIndex
-import index
+import mockIndex
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
+import searchView
 import shouldEqual
 
 
@@ -21,29 +22,15 @@ import shouldEqual
 @RunWith(AndroidJUnit4::class)
 @Config(manifest=Config.NONE)
 class TestClearFilter {
-
-    private fun searchView() = SearchView(ApplicationProvider.getApplicationContext())
-    private fun clearFilterView() = object : ClearFilterView {
-        public val view = View(ApplicationProvider.getApplicationContext())
-        override var onClick: (() -> Unit)? = null
-            set(value) {
-                field = value
-                view.setOnClickListener {
-                    field?.invoke()
-                }
-            }
-    }
-
     @Test
     fun clearAllClearsSearchViewQuery() {
         val searchView = searchView()
         val clearFilterView = clearFilterView()
-        val searcher = SearcherSingleIndex(index)
+        val searcher = SearcherSingleIndex(mockIndex)
 
-        //TODO move this logic to connector once SB PR is merged
-        searcher.onQueryChanged += { text : String? ->
-            searchView.setQuery(text, false)
-        }
+        val searchBoxViewModel = SearchBoxViewModel()
+        searchBoxViewModel.connectSearchView(searchView)
+        searchBoxViewModel.connectSearcher(searcher)
 
         val viewModel = ClearFilterViewModel()
         viewModel.connectState(searcher.filterState, searcher)
