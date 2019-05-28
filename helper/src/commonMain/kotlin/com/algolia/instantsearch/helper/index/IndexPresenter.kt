d32d7ba879171432ec469b7ca94aa2104adab854
Quentin LITZLER <qlitzler@gmail.com>
2019-05-28 11:20:44 +0200

    IndexPresenter

---
+++ helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/index/IndexPresenter.kt
@@ -0,0 +1,6 @@
+package com.algolia.instantsearch.helper.index
+
+import com.algolia.search.client.Index
+
+
+typealias IndexPresenter = (Index) -> String
--- helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/index/IndexSegmentConnectView.kt
+++ helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/index/IndexSegmentConnectView.kt
@@ -1,12 +1,11 @@
 package com.algolia.instantsearch.helper.index
 
 import com.algolia.instantsearch.core.selectable.segment.SelectableSegmentView
-import com.algolia.search.client.Index
 
 
 public fun IndexSegmentViewModel.connectView(
     view: SelectableSegmentView<Int, String>,
-    presenter: (Index) -> String
+    presenter: IndexPresenter
 ) {
     view.setItems(items.map { it.key to presenter(it.value) }.toMap())
     view.setSelected(selected)
