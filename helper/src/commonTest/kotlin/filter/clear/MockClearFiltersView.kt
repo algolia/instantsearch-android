5661e6a3d17dc6e9ecd3f9a8ffbbb487cc916774
Paul-Louis NECH <pln@algolia.com>
2019-05-29 11:27:27 +0200

    refactor: Inline MockClearFiltersView

--- helper/src/commonTest/kotlin/filter/clear/MockClearFiltersView.kt
+++
@@ -1,7 +0,0 @@
-package filter.clear
-
-import com.algolia.instantsearch.helper.filter.clear.ClearFiltersView
-
-class MockClearFiltersView : ClearFiltersView {
-    override var onClick: (() -> Unit)? = null
-}
--- helper/src/commonTest/kotlin/filter/clear/TestClearFiltersConnectView.kt
+++ helper/src/commonTest/kotlin/filter/clear/TestClearFiltersConnectView.kt
@@ -1,11 +1,18 @@
 package filter.clear
 
+import com.algolia.instantsearch.helper.filter.clear.ClearFiltersView
 import com.algolia.instantsearch.helper.filter.clear.ClearFiltersViewModel
 import com.algolia.instantsearch.helper.filter.clear.connectView
 import shouldNotEqual
 import kotlin.test.Test
 
+
 class TestClearFiltersConnectView {
+
+    private class MockClearFiltersView : ClearFiltersView {
+        override var onClick: (() -> Unit)? = null
+    }
+
     @Test
     fun connectShouldSetOnClick() {
         val viewModel = ClearFiltersViewModel()
@@ -13,4 +20,5 @@ class TestClearFiltersConnectView {
         viewModel.connectView(view)
         view.onClick shouldNotEqual null
     }
+
 }
