01011d5281e4ae7c2e8e6449315b0bd3e32a20e2
Paul-Louis NECH <pln@algolia.com>
2019-05-29 11:59:01 +0200

    refactor: remove common mocks for now

--- helper/src/androidTest/kotlin/InstrumentationTests.kt
+++ helper/src/androidTest/kotlin/InstrumentationTests.kt
@@ -1,10 +1,5 @@
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
-import com.algolia.search.model.IndexName
-import com.algolia.search.model.response.ResponseSearch
 
-private val mockResponse = ResponseSearch()
-private val mockClient = mockClient(mockResponse, ResponseSearch.serializer())
-public val mockIndex = mockClient.initIndex(IndexName("stub"))
 public val applicationContext: Context = ApplicationProvider.getApplicationContext()
