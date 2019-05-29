f60c30d402bfdbbbfa9a4d2429f0d76b003e0305
Paul-Louis NECH <pln@algolia.com>
2019-05-29 16:03:59 +0200

    style: Useless this

--- helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/filter/clear/ClearFilterConnectView.kt
+++ helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/filter/clear/ClearFilterConnectView.kt
@@ -3,6 +3,6 @@ package com.algolia.instantsearch.helper.filter.clear
 
 public fun ClearFiltersViewModel.connectView(view: ClearFiltersView) {
     view.onClick = {
-        this.clearFilters()
+        clearFilters()
     }
 }
