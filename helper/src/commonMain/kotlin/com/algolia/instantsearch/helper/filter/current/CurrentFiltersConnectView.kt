955198c010a72ac9afbd836c23ab89f5f42d25ce
Quentin LITZLER <qlitzler@gmail.com>
2019-06-05 17:54:46 +0200

    Move connectView to core, with tests

--- helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/filter/current/CurrentFiltersConnectView.kt
+++
@@ -1,9 +0,0 @@
-package com.algolia.instantsearch.helper.filter.current
-
-import com.algolia.instantsearch.core.item.connectView
-
-
-public fun CurrentFiltersViewModel.connectView(view: CurrentFiltersView) {
-    connectView(view) { it }
-    view.onClick  = { remove(it) }
-}
