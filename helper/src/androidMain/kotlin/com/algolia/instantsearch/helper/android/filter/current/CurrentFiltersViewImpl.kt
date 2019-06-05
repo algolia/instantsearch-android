e94b42f1ca023e30a89fcf9be4f6f740eb499898
Paul-Louis NECH <pln@algolia.com>
2019-06-05 15:25:35 +0200

    feat(CurrentFilters): ViewImpl first draft

--- helper/src/androidMain/kotlin/com/algolia/instantsearch/helper/android/filter/current/CurrentFiltersViewImpl.kt
+++ helper/src/androidMain/kotlin/com/algolia/instantsearch/helper/android/filter/current/CurrentFiltersViewImpl.kt
@@ -1,2 +1,37 @@
 package com.algolia.instantsearch.helper.android.filter.current
 
+import android.view.View
+import com.algolia.instantsearch.helper.filter.current.CurrentFiltersView
+import com.algolia.search.model.filter.Filter
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+
+
+public class CurrentFiltersViewImpl(
+    val view: ChipGroup
+) : CurrentFiltersView {
+
+    var filters = sortedMapOf<String, Filter>()
+
+    override var onClick: ((String) -> Unit)? = null
+
+    override fun setItems(items: Map<String, Filter>) {
+        filters = items.toSortedMap()
+        updateView()
+    }
+
+    private fun updateView() {
+        view.removeAllViews()
+        filters.entries.forEach {
+            val identifier = it.key
+            val filter = it.value
+            val chip = Chip(view.context)
+
+            chip.text = filter.toString()
+            chip.setOnClickListener(View.OnClickListener {
+                onClick?.invoke(identifier)
+            })
+            view.addView(chip)
+        }
+    }
+}
--- helper/src/androidTest/kotlin/filter/current/TestCurrentFiltersViewImpl.kt
+++ helper/src/androidTest/kotlin/filter/current/TestCurrentFiltersViewImpl.kt
@@ -1,13 +1,13 @@
-package filter.clear
+package filter.current
 
 import android.view.View
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import applicationContext
-import com.algolia.instantsearch.helper.android.filter.clear.FilterClearViewImpl
-import com.algolia.instantsearch.helper.filter.clear.FilterClearViewModel
-import com.algolia.instantsearch.helper.filter.clear.connectFilterState
-import com.algolia.instantsearch.helper.filter.clear.connectView
+import com.algolia.instantsearch.helper.android.filter.current.CurrentFiltersViewImpl
+import com.algolia.instantsearch.helper.filter.current.CurrentFiltersViewModel
+import com.algolia.instantsearch.helper.filter.current.connectFilterState
+import com.algolia.instantsearch.helper.filter.current.connectView
 import com.algolia.instantsearch.helper.filter.state.FilterGroupID
 import com.algolia.instantsearch.helper.filter.state.FilterState
 import com.algolia.search.model.Attribute
@@ -22,7 +22,7 @@ import shouldNotBeEmpty
 @SmallTest
 @RunWith(AndroidJUnit4::class)
 @Config(manifest = Config.NONE)
-class TestFilterClearViewImpl {
+class TestCurrentFiltersViewImpl {
 
     private val color = Attribute("color")
     private val groupID = FilterGroupID(color)
@@ -33,11 +33,12 @@ class TestFilterClearViewImpl {
     @Test
     fun onViewClickCallsClearFilters() {
         val filterState = FilterState(mapOf(groupID to setOf(filterRed)))
-        val viewModel = FilterClearViewModel()
-        val view = FilterClearViewImpl(view())
+        val viewModel = CurrentFiltersViewModel()
+        val view = CurrentFiltersViewImpl(view())
 
         viewModel.connectFilterState(filterState)
         viewModel.connectView(view)
+
         filterState.getFilters().shouldNotBeEmpty()
         view.view.callOnClick()
         filterState.getFilters().shouldBeEmpty()
