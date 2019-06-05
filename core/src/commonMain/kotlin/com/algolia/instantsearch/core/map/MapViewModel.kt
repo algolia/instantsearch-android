afd594241957eba20c1d48fd50feab92cce0b995
Quentin LITZLER <qlitzler@gmail.com>
2019-06-05 17:39:58 +0200

    Add public on MapViewModel

--- core/src/commonMain/kotlin/com/algolia/instantsearch/core/map/MapViewModel.kt
+++ core/src/commonMain/kotlin/com/algolia/instantsearch/core/map/MapViewModel.kt
@@ -7,14 +7,15 @@ public open class MapViewModel<K, V>(
     items: Map<K, V> = mapOf()
 ) : ItemViewModel<Map<K, V>>(items) {
 
-    var map: Map<K, V>
+    public var map: Map<K, V>
         get() = item
         set(value) {
             item = value
         }
-    val onMapComputed: MutableList<(Map<K, V>) -> Unit> = mutableListOf()
 
-    fun remove(key: K) {
+    public val onMapComputed: MutableList<(Map<K, V>) -> Unit> = mutableListOf()
+
+    public fun remove(key: K) {
         val map = item.toMutableMap().apply { remove(key) }
 
         onMapComputed.forEach { it(map) }
