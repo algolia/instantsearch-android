51761160b4b990ef62b404ce5f486fbd72f9d4f4
Quentin LITZLER <qlitzler@gmail.com>
2019-06-09 18:58:33 +0200

    Loading debouncer

--- demo/src/main/kotlin/com/algolia/instantsearch/demo/loading/LoadingDemo.kt
+++ demo/src/main/kotlin/com/algolia/instantsearch/demo/loading/LoadingDemo.kt
@@ -2,15 +2,22 @@ package com.algolia.instantsearch.demo.loading
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
 import com.algolia.instantsearch.core.loading.LoadingViewModel
 import com.algolia.instantsearch.core.loading.connectView
+import com.algolia.instantsearch.core.searchbox.SearchBoxViewModel
+import com.algolia.instantsearch.core.searchbox.connectView
 import com.algolia.instantsearch.demo.*
 import com.algolia.instantsearch.demo.list.movie.Movie
-import com.algolia.instantsearch.demo.list.movie.MovieAdapter
+import com.algolia.instantsearch.demo.list.movie.MovieAdapterPaged
+import com.algolia.instantsearch.helper.android.list.SearcherSingleIndexDataSource
 import com.algolia.instantsearch.helper.android.loading.LoadingViewSwipeRefreshLayout
+import com.algolia.instantsearch.helper.android.searchbox.SearchBoxViewAppCompat
+import com.algolia.instantsearch.helper.android.searchbox.connectSearcher
 import com.algolia.instantsearch.helper.loading.connectSearcher
 import com.algolia.instantsearch.helper.searcher.SearcherSingleIndex
-import com.algolia.search.helper.deserialize
 import kotlinx.android.synthetic.main.demo_loading.*
 import kotlinx.android.synthetic.main.demo_search.list
 import kotlinx.android.synthetic.main.demo_search.toolbar
@@ -20,6 +27,9 @@ import kotlinx.android.synthetic.main.include_search.*
 class LoadingDemo : AppCompatActivity() {
 
     private val searcher = SearcherSingleIndex(stubIndex)
+    private val dataSourceFactory = SearcherSingleIndexDataSource.Factory(searcher, Movie.serializer())
+    private val pagedListConfig = PagedList.Config.Builder().setPageSize(10).build()
+    private val movies = LivePagedListBuilder<Int, Movie>(dataSourceFactory, pagedListConfig).build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,15 +41,18 @@ class LoadingDemo : AppCompatActivity() {
         viewModel.connectView(view)
         viewModel.connectSearcher(searcher)
 
-        val adapter = MovieAdapter()
+        val adapter = MovieAdapterPaged()
 
-        searcher.onResponseChanged += {
-            adapter.submitList(it.hits.deserialize(Movie.serializer()))
-        }
+        val searchBoxViewModel = SearchBoxViewModel()
+        val searchBoxView = SearchBoxViewAppCompat(searchView)
+
+        movies.observe(this, Observer { hits -> adapter.submitList(hits) })
+
+        searchBoxViewModel.connectView(searchBoxView)
+        searchBoxViewModel.connectSearcher(searcher, movies)
 
         configureSearcher(searcher)
         configureToolbar(toolbar)
-        configureSearchBox(searchView, searcher)
         configureSearchView(searchView, getString(R.string.search_movies))
         configureRecyclerView(list, adapter)
 
--- helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/loading/LoadingConnectSearcher.kt
+++ helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/loading/LoadingConnectSearcher.kt
@@ -1,11 +1,16 @@
 package com.algolia.instantsearch.helper.loading
 
 import com.algolia.instantsearch.core.loading.LoadingViewModel
+import com.algolia.instantsearch.core.searcher.Debouncer
 import com.algolia.instantsearch.core.searcher.Searcher
 
 
-public fun <R> LoadingViewModel.connectSearcher(searcher: Searcher<R>) {
+public fun <R> LoadingViewModel.connectSearcher(searcher: Searcher<R>, debouncer: Debouncer = Debouncer(200)) {
     item = searcher.loading
     onTriggered += { searcher.searchAsync() }
-    searcher.onLoadingChanged += { loading -> item = loading }
+    searcher.onLoadingChanged += { loading ->
+        debouncer.debounce(searcher) {
+            item = loading
+        }
+    }
 }
