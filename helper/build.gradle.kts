7a46a51f690124e0c91606b8bbc6c4b2d4d201ee
Quentin LITZLER <qlitzler@gmail.com>
2019-06-09 23:30:11 +0200

    Bump minSDK to 17

--- helper/build.gradle.kts
+++ helper/build.gradle.kts
@@ -23,7 +23,7 @@ android {
     compileSdkVersion(28)
 
     defaultConfig {
-        minSdkVersion(16)
+        minSdkVersion(17)
         targetSdkVersion(28)
         versionCode = 1
         versionName = "1.0"
