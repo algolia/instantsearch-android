5a7206fc5b06a0e0935bcd757d9877e078ba57f5
Paul-Louis NECH <pln@algolia.com>
2019-06-14 17:09:11 +0200

    feat(NumericRangeDemo): New activity

---
+++ demo/src/main/kotlin/com/algolia/instantsearch/demo/filter/range/FilterRangeDemo.kt
@@ -0,0 +1,14 @@
+package com.algolia.instantsearch.demo.filter.range
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.algolia.instantsearch.demo.*
+
+
+class FilterRangeDemo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.demo_filter_range)
+    }
+}
