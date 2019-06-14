e2ac7b421421f797a82eb820d55c7f6559893944
Paul-Louis NECH <pln@algolia.com>
2019-06-14 16:30:44 +0200

    chore: remove superfluous this

--- core/src/commonMain/kotlin/com/algolia/instantsearch/core/number/range/NumberRangeConnectView.kt
+++ core/src/commonMain/kotlin/com/algolia/instantsearch/core/number/range/NumberRangeConnectView.kt
@@ -8,5 +8,5 @@ public fun <T : Number> NumberRangeViewModel<T>.connectView(
     view: NumberRangeView<T>
 ) {
     connectItemView(view) { it }
-    connectEventView(view) { range?.let { this.trigger(it) } }
+    connectEventView(view) { range?.let { trigger(it) } }
 }
