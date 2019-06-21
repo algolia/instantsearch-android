2ef0ad0d71862a8e2f2bf71bd95d09a635633a5a
Paul-Louis NECH <pln@algolia.com>
2019-06-21 13:13:11 +0200

    feat(Highlighter): First draft

--- helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/highlighting/Highlighter.kt
+++ helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/highlighting/Highlighter.kt
@@ -1,2 +1,25 @@
 package com.algolia.instantsearch.helper.highlighting
 
+import com.algolia.instantsearch.core.highlighting.HighlightTokenizer
+import com.algolia.instantsearch.core.highlighting.HighlightedString
+import com.algolia.search.model.Attribute
+import com.algolia.search.model.response.ResponseSearch
+import kotlinx.serialization.json.JsonObject
+
+
+class Highlighter {
+
+    fun getHighlight(
+        attribute: Attribute,
+        hit: ResponseSearch.Hit
+    ): HighlightedString? {
+        val highlightResult: JsonObject = hit.highlightResult
+        val highlightedAttribute = highlightResult[attribute.raw]
+
+        highlightedAttribute?.let {
+            val value = it.jsonObject["value"]
+            return HighlightTokenizer().tokenize(value.toString())
+        }
+        return null
+    }
+}
--- helper/src/commonTest/kotlin/highlighting/TestHighlighter.kt
+++ helper/src/commonTest/kotlin/highlighting/TestHighlighter.kt
@@ -1,2 +1,46 @@
 package highlighting
 
+import com.algolia.instantsearch.helper.highlighting.Highlighter
+import com.algolia.search.model.Attribute
+import com.algolia.search.model.response.ResponseSearch
+import com.algolia.search.model.search.HighlightResult
+import com.algolia.search.model.search.MatchLevel
+import com.algolia.search.serialize.Key_HighlightResult
+import kotlinx.serialization.internal.HashMapSerializer
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import kotlinx.serialization.json.json
+import shouldEqual
+import shouldNotBeNull
+import kotlin.test.Test
+
+
+class TestHighlighter {
+
+    private val Json = Json(JsonConfiguration.Stable.copy())
+
+    private val attribute = Attribute("name")
+    private val highlightResult = HighlightResult("<em>John</em>ny", MatchLevel.Partial, listOf("Johnny"))
+    private val highlights = mapOf(attribute to highlightResult)
+    private val hit = ResponseSearch.Hit(json {
+        attribute.raw to "Johnny"
+        "age" to 42
+        Key_HighlightResult to Json.toJson(HashMapSerializer(Attribute, HighlightResult.serializer()), highlights)
+    })
+
+    @Test
+    fun getHighlight() {
+        val highlight = Highlighter().getHighlight(attribute, hit)
+
+        highlight.shouldNotBeNull()
+        highlight?.let {
+            highlight.parts.size shouldEqual 2
+            highlight.highlightedParts shouldEqual listOf("John")
+
+            println("${highlight.parts.size} parts:")
+            highlight.parts.forEach {
+                println("${it.content.length}: $it")
+            }
+        }
+    }
+}
