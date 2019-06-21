74ec972344540aff028d4dc73b5ce5670008fb93
Paul-Louis NECH <pln@algolia.com>
2019-06-21 12:17:17 +0200

    chore(HighlightedString): Formatting

--- core/src/commonMain/kotlin/com/algolia/instantsearch/core/highlighting/HighlightedString.kt
+++ core/src/commonMain/kotlin/com/algolia/instantsearch/core/highlighting/HighlightedString.kt
@@ -1,8 +1,11 @@
 package com.algolia.instantsearch.core.highlighting
-    data class HighlightedString(val original: String, val parts: List<HighlightToken>) {
 
-        public val highlightedParts
-            get() = parts.filter { it.highlighted }.map { it.content }
 
-        override fun toString(): String = parts.map { if (it.highlighted) "_${it.content}_" else it.content }.toString()
-    }
+data class HighlightedString(val original: String, val parts: List<HighlightToken>) {
+
+    public val highlightedParts
+        get() = parts.filter { it.highlighted }.map { it.content }
+
+    override fun toString(): String = parts
+        .map { if (it.highlighted) "_${it.content}_" else it.content }.toString()
+}
