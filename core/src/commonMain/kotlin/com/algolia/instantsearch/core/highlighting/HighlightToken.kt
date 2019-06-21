b190249c138bd64dbe3d6fe0722237f6264d7f9e
Paul-Louis NECH <pln@algolia.com>
2019-06-21 12:57:38 +0200

    chore(HighlightToken): Better toString

--- core/src/commonMain/kotlin/com/algolia/instantsearch/core/highlighting/HighlightToken.kt
+++ core/src/commonMain/kotlin/com/algolia/instantsearch/core/highlighting/HighlightToken.kt
@@ -4,4 +4,6 @@ package com.algolia.instantsearch.core.highlighting
 /**
  * A token from a highlighted string.
  */
-public data class HighlightToken(val content: String, val highlighted: Boolean = false)
+public data class HighlightToken(val content: String, val highlighted: Boolean = false) {
+    override fun toString(): String = if (highlighted) "_${content}_" else content
+}
