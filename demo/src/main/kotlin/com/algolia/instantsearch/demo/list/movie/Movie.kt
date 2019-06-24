21b49ba0843a9d8ee881f6d08388cfe57219b2bc
Paul-Louis NECH <pln@algolia.com>
2019-06-24 16:01:55 +0200

    feat(Movie{Adapter}): Use Highlighter (WIP)

--- demo/src/main/kotlin/com/algolia/instantsearch/demo/list/movie/Movie.kt
+++ demo/src/main/kotlin/com/algolia/instantsearch/demo/list/movie/Movie.kt
@@ -1,8 +1,14 @@
 package com.algolia.instantsearch.demo.list.movie
 
+import com.algolia.instantsearch.helper.highlighting.Highlighter
+import com.algolia.search.model.Attribute
 import com.algolia.search.model.ObjectID
 import com.algolia.search.model.indexing.Indexable
+import com.algolia.search.serialize.Key_HighlightResult
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+import kotlinx.serialization.json.JsonObject
 
 
 @Serializable
@@ -11,5 +17,11 @@ data class Movie(
     val year: String,
     val genre: List<String>,
     val image: String,
+    @SerialName(Key_HighlightResult) private val highlightResult: JsonObject? = null,
     override val objectID: ObjectID
-) : Indexable
+) : Indexable {
+
+    @Transient
+    public val titleHighlight
+        get() = Highlighter().getHighlight(Attribute("title"), highlightResult)
+}
--- demo/src/main/kotlin/com/algolia/instantsearch/demo/list/movie/MovieViewHolder.kt
+++ demo/src/main/kotlin/com/algolia/instantsearch/demo/list/movie/MovieViewHolder.kt
@@ -1,8 +1,9 @@
 package com.algolia.instantsearch.demo.list.movie
 
 import android.view.View
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
 import androidx.recyclerview.widget.RecyclerView
-import com.algolia.instantsearch.demo.R
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.list_item_large.view.*
 
@@ -10,7 +11,25 @@ import kotlinx.android.synthetic.main.list_item_large.view.*
 class MovieViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
     fun bind(movie: Movie) {
-        view.itemTitle.text = view.resources.getString(R.string.template_title).format(movie.title, movie.year)
+        val title = buildSpannedString {
+            val parts = movie.titleHighlight?.parts
+
+            parts?.forEach { (isHighlighted, part) ->
+                if (isHighlighted) {
+                    println("High part: $part")
+                    bold { append(part) }
+                } else {
+                    println("Normal part: $part")
+                    append(part)
+                }
+            } ?: append(movie.title)
+            append("(${movie.year})")
+        }
+
+        //TODO: Move to own demo, not paging's
+        //FIXME: Is there a way we can still use String.format?
+        //view.itemTitle.text = view.resources.getString(R.string.template_title).format(title, movie.year)
+        view.itemTitle.text = title
         view.itemSubtitle.text = movie.genre.sorted().joinToString { it }
         Glide.with(view)
             .load(movie.image).placeholder(android.R.drawable.ic_media_play)
