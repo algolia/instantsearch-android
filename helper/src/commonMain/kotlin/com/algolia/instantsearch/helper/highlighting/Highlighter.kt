4cc5e6f618bcbfbde5a7e543f4260943983d34a0
Paul-Louis NECH <pln@algolia.com>
2019-06-24 14:23:53 +0200

    feat(Highlighter): Allow null _highlightResult

--- helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/highlighting/Highlighter.kt
+++ helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/highlighting/Highlighter.kt
@@ -4,17 +4,27 @@ import com.algolia.instantsearch.core.highlighting.HighlightTokenizer
 import com.algolia.instantsearch.core.highlighting.HighlightedString
 import com.algolia.search.model.Attribute
 import com.algolia.search.model.response.ResponseSearch
+import com.algolia.search.model.search.HighlightResult
 import kotlinx.serialization.json.content
 
 
+fun HighlightResult.toHighlightedString(): HighlightedString {
+    return HighlightTokenizer().tokenize(value)
+}
+
 class Highlighter {
 
+    /**
+     * Creates a [HighlightedString] from the [hit]'s [attribute].
+     *
+     * @return null if no `_highlightResult` was found.
+     */
     fun getHighlight(
         attribute: Attribute,
         hit: ResponseSearch.Hit
     ): HighlightedString? {
 
-        return hit.highlightResult[attribute.raw]?.jsonObject?.get("value")?.content?.let {
+        return hit.highlightResultOrNull?.get(attribute.raw)?.jsonObject?.get("value")?.content?.let {
             HighlightTokenizer().tokenize(it)
         }
     }
