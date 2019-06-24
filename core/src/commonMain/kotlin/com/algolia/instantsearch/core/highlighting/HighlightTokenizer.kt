df05553dcf8ec6d34b6adb72856958b3ce116fa2
Paul-Louis NECH <pln@algolia.com>
2019-06-24 16:48:10 +0200

    style(HighlightTokenizer): prefer findAll+forEach over while loop

--- core/src/commonMain/kotlin/com/algolia/instantsearch/core/highlighting/HighlightTokenizer.kt
+++ core/src/commonMain/kotlin/com/algolia/instantsearch/core/highlighting/HighlightTokenizer.kt
@@ -7,36 +7,29 @@ private const val DefaultPostTag: String = "</em>"
 public class HighlightTokenizer(
     public val preTag: String = DefaultPreTag,
     public val postTag: String = DefaultPostTag
-) : ((String) -> HighlightedString){
+) : ((String) -> HighlightedString) {
 
     private val regex = Regex(Regex.escape(preTag) + ".*?" + Regex.escape(postTag))
 
-    override fun invoke(p1: String): HighlightedString {
-        return HighlightedString(p1, mutableListOf<HighlightToken>().apply {
-            var startIndex = 0
-            var matchResult = regex.find(p1)
+    override fun invoke(p1: String): HighlightedString = HighlightedString(p1, mutableListOf<HighlightToken>().apply {
+        var startIndex = 0
 
-            while (matchResult != null && matchResult.groups.isNotEmpty()) {
-                matchResult.groups[0]?.let {
-                    val groupStart = p1.indexOf(it.value, startIndex)
-                    val groupEnd = groupStart + it.value.length
-                    val textBeforeHighlight = p1.substring(startIndex, groupStart)
-                    val textHighlighted = p1.substring(groupStart + preTag.length, groupEnd - postTag.length)
+        regex.findAll(p1).forEach {
+            val groupStart = p1.indexOf(it.value, startIndex)
+            val groupEnd = groupStart + it.value.length
+            val textBeforeHighlight = p1.substring(startIndex, groupStart)
+            val textHighlighted = p1.substring(groupStart + preTag.length, groupEnd - postTag.length)
 
-                    if (groupStart != startIndex) { // There was unmatched input before this match
-                        add(HighlightToken(textBeforeHighlight, false))
-                    }
-                    add(HighlightToken(textHighlighted, true))
-
-                    startIndex = groupEnd
-                }
-
-                matchResult = matchResult.next()
+            if (groupStart != startIndex) { // There was unmatched input before this match
+                add(HighlightToken(textBeforeHighlight, false))
             }
+            add(HighlightToken(textHighlighted, true))
 
-            if (startIndex != p1.length) { // Some input remains after the last match
-                add(HighlightToken(p1.substring(startIndex), false))
-            }
-        })
-    }
+            startIndex = groupEnd
+        }
+
+        if (startIndex != p1.length) { // Some input remains after the last match
+            add(HighlightToken(p1.substring(startIndex), false))
+        }
+    })
 }
