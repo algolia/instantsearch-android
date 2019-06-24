46221c476a724a6d3c2c2c5dfe1c5c90f3386a1c
Paul-Louis NECH <pln@algolia.com>
2019-06-24 16:42:32 +0200

    refactor(TestHighlightTokenizer): factorize + test start/end

--- core/src/commonTest/kotlin/highlighting/TestHighlightTokenizer.kt
+++ core/src/commonTest/kotlin/highlighting/TestHighlightTokenizer.kt
@@ -1,7 +1,6 @@
 package highlighting
 
 import com.algolia.instantsearch.core.highlighting.HighlightTokenizer
-import shouldBeEmpty
 import shouldBeFalse
 import shouldBeTrue
 import shouldEqual
@@ -10,48 +9,67 @@ import kotlin.test.Test
 
 class TestHighlightTokenizer {
 
+    /*
+    * matchStart
+    * matchEnd
+    * matchMiddle
+    * */
+
     @Test
-    fun match() {
-        val tokenizer = HighlightTokenizer()
-        val input = "This <em>John</em> Doe looks like <em>John</em>athan."
+    fun matchNone() = shouldMatch(
+        "This John Doe looks like Johnathan.",
+        1
+    )
+
+    @Test
+    fun matchAll() = shouldMatch(
+        "<em>John</em><em>John</em>",
+        2, listOf("John", "John")
+    )
+
+    @Test
+    fun matchStart() = shouldMatch(
+        "<em>John</em> is a nice guy.",
+        2, listOf("John")
+    )
+
+    @Test
+    fun matchEnd() = shouldMatch(
+        "I love <em>John</em>",
+        2, listOf("John")
+    )
+
+    @Test
+    fun matchTwiceMiddle() = shouldMatch(
+        "This <em>John</em> Doe looks like <em>John</em>athan.",
+        5, listOf("John", "John")
+    )
+
+    @Test
+    fun matchWithCustomTags() =
+        shouldMatch(
+            "This ^Game$ is really ^funny$, don't you ^think$?",
+            7, listOf("Game", "funny", "think"),
+            Pair("^", "$")
+        )
+
+    private fun shouldMatch(
+        input: String,
+        expectNbParts: Int = 1,
+        expectHighlightedParts: List<String> = listOf(),
+        tags: Pair<String, String>? = null
+    ) {
+        val tokenizer = if (tags != null) HighlightTokenizer(tags.first, tags.second) else HighlightTokenizer()
         val output = tokenizer(input)
 
         output.original shouldEqual input
-        output.parts.size shouldEqual 5
-        output.highlightedParts shouldEqual listOf("John", "John")
-    }
+        output.parts.size shouldEqual expectNbParts
+        output.highlightedParts shouldEqual expectHighlightedParts
 
-    @Test
-    fun noMatch() {
-        val tokenizer = HighlightTokenizer()
-        val input = "This John Doe looks like Johnathan."
-        val output = tokenizer(input)
-
-        output.original shouldEqual input
-        output.parts.size shouldEqual 1
-        output.parts.any { it.highlighted }.shouldBeFalse()
-        output.highlightedParts.shouldBeEmpty()
-    }
-
-    @Test
-    fun onlyMatch() {
-        val tokenizer = HighlightTokenizer()
-        val input = "<em>John</em><em>John</em>"
-        val output = tokenizer(input)
-
-        output.original shouldEqual input
-        println(output.parts)
-        output.parts.size shouldEqual 2
-        output.parts.all { it.highlighted }.shouldBeTrue()
-        output.highlightedParts shouldEqual listOf("John", "John")
-    }
-
-    @Test
-    fun customTags() {
-        val tokenizer = HighlightTokenizer("^", "$")
-        val output = tokenizer("This ^Game$ is really ^funny$, don't you ^think$?")
-
-        output.parts.size shouldEqual 7
-        output.highlightedParts shouldEqual listOf("Game", "funny", "think")
+        if (expectHighlightedParts.isEmpty()) { // No part should be highlighted
+            output.parts.any { it.highlighted }.shouldBeFalse()
+        } else if (expectNbParts == expectHighlightedParts.size) { // All parts should be highlighted
+            output.parts.all { it.highlighted }.shouldBeTrue()
+        }
     }
 }
