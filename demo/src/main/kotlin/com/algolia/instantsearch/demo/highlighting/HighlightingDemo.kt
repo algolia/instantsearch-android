b7fe3c4a39d4d65581f20457846e0dd8518376d0
Paul-Louis NECH <pln@algolia.com>
2019-06-26 19:06:59 +0200

    feat(HighlightDemo): First draft

---
+++ demo/src/main/kotlin/com/algolia/instantsearch/demo/highlighting/HighlightingDemo.kt
@@ -0,0 +1,45 @@
+package com.algolia.instantsearch.demo.highlighting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.algolia.instantsearch.core.searchbox.SearchBoxViewModel
+import com.algolia.instantsearch.core.searchbox.connectView
+import com.algolia.instantsearch.demo.*
+import com.algolia.instantsearch.demo.list.movie.Movie
+import com.algolia.instantsearch.demo.list.movie.MovieAdapter
+import com.algolia.instantsearch.helper.android.searchbox.SearchBoxViewAppCompat
+import com.algolia.instantsearch.helper.searchbox.connectSearcher
+import com.algolia.instantsearch.helper.searcher.SearcherSingleIndex
+import com.algolia.search.helper.deserialize
+import kotlinx.android.synthetic.main.demo_search.*
+import kotlinx.android.synthetic.main.include_search.*
+
+
+class HighlightingDemo : AppCompatActivity() {
+
+    private val searcher = SearcherSingleIndex(stubIndex)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.demo_highlighting)
+
+        val adapter = MovieAdapter()
+        val searchBoxViewModel = SearchBoxViewModel()
+        val searchBoxView = SearchBoxViewAppCompat(searchView)
+
+        searchBoxViewModel.connectView(searchBoxView)
+        searchBoxViewModel.connectSearcher(searcher)
+
+        searcher.onResponseChanged += {
+            adapter.submitList(it.hits.deserialize(Movie.serializer()))
+        }
+
+        configureToolbar(toolbar)
+        configureSearcher(searcher)
+        configureRecyclerView(list, adapter)
+        configureSearchView(searchView, getString(R.string.search_movies))
+
+        searcher.searchAsync()
+
+    }
+}
