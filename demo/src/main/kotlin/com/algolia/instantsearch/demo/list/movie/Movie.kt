42c8c431f1254d3ac1f823acbbc4c8dd807eee7a
Paul-Louis NECH <pln@algolia.com>
2019-06-27 15:15:30 +0200

    refactor(Movie): Composition with Highlightable

--- demo/src/main/kotlin/com/algolia/instantsearch/demo/list/movie/Movie.kt
+++ demo/src/main/kotlin/com/algolia/instantsearch/demo/list/movie/Movie.kt
@@ -5,31 +5,41 @@ import com.algolia.instantsearch.helper.highlighting.Highlighter
 import com.algolia.search.model.Attribute
 import com.algolia.search.model.ObjectID
 import com.algolia.search.model.indexing.Indexable
-import com.algolia.search.serialize.Key_HighlightResult
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 import kotlinx.serialization.json.JsonObject
 
 /* FIXME: How can we provide a simple API to users that use @Serializable objects?
 
-   - 1st idea: private val highlightResult + highlights get() = Highlighter.getHighlights(highlightResult)
+   - 1st idea: private val _highlightResult + highlights get() = Highlighter.getHighlights(_highlightResult)
 
-   - 2nd idea: abstract Hit with highlightResult + highlights, then Movie(objectID): Hit(objectID)
+   - 2nd idea: abstract Hit with _highlightResult + highlights, then Movie(objectID): Hit(objectID)
 */
-@Serializable
-abstract class Hit(
-
-    //DISCUSS: can this fake objectID cause problems down the line?
-    @Transient override val objectID: ObjectID = ObjectID("stubObjectID")
-) : Indexable {
-    @SerialName(Key_HighlightResult)
-
-    private val highlightResult: JsonObject? = null
+interface Highlightable {
+    @Suppress("PropertyName") // Else implementers have to specify @SerialName
+    val _highlightResult: JsonObject?
 
     @Transient
     public val highlights: Map<Attribute, List<HighlightedString>>?
-        get() = Highlighter.getHighlights(highlightResult)
+        get() {
+            val highlights1 = Highlighter.getHighlights(_highlightResult)
+            println("HS: $highlights1")
+            return highlights1
+        }
+
+    fun getHighlightsOrNull(attribute: Attribute): List<HighlightedString>? = highlights?.get(attribute)
+
+    fun getHighlightOrNull(attribute: Attribute): HighlightedString? = highlights?.get(attribute)?.first()
+
+    fun getHighlights(attribute: Attribute): List<HighlightedString> =
+        getHighlightsOrNull(attribute) ?: throw IllegalStateException(
+            "No highlights for attribute \"$attribute\". Is it in `searchableAttributes`, and in `attributesToHighlight` if specified?"
+        )
+
+    fun getHighlight(attribute: Attribute): HighlightedString =
+        getHighlightOrNull(attribute) ?: throw IllegalStateException(
+            "No highlight for attribute \"$attribute\". Is it in `searchableAttributes`, and in `attributesToHighlight` if specified?"
+        )
 }
 
 
@@ -39,11 +49,21 @@ data class Movie(
     val year: String,
     val genre: List<String>,
     val image: String,
-    override val objectID: ObjectID
-) : Hit(objectID) {
+    override val objectID: ObjectID,
+    override val _highlightResult: JsonObject?
+) : Indexable, Highlightable {
+
+    init {
+        if (objectID.raw == "stubObjectID") {
+            throw IllegalStateException("Stub objID!")
+        }
+    }
 
     @Transient
-    public val titleHighlight
-        get() = highlights?.get(Attribute("title"))?.first()
+    public val highlightedTitle
+        get() = getHighlight(Attribute("title"))
 
+    @Transient
+    public val highlightedGenres: List<HighlightedString>
+        get() = if (genre.isNotEmpty()) getHighlights(Attribute("genre")) else emptyList()
 }
