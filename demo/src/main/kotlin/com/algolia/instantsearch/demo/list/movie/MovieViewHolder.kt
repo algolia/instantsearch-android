9e7cd2c289a5e04ee26ed6dbe16a3fcc94714f92
Paul-Louis NECH <pln@algolia.com>
2019-06-27 15:18:40 +0200

    feat(MovieViewHolder): Highlighted genres

--- demo/src/main/kotlin/com/algolia/instantsearch/demo/list/movie/MovieViewHolder.kt
+++ demo/src/main/kotlin/com/algolia/instantsearch/demo/list/movie/MovieViewHolder.kt
@@ -1,9 +1,12 @@
 package com.algolia.instantsearch.demo.list.movie
 
+import android.text.SpannedString
+import android.text.TextUtils
+import android.text.style.StyleSpan
 import android.view.View
-import androidx.core.text.bold
-import androidx.core.text.buildSpannedString
+import androidx.core.text.getSpans
 import androidx.recyclerview.widget.RecyclerView
+import com.algolia.instantsearch.helper.highlighting.toSpannedString
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.list_item_large.view.*
 
@@ -11,26 +14,19 @@ import kotlinx.android.synthetic.main.list_item_large.view.*
 class MovieViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
     fun bind(movie: Movie) {
-        val title = buildSpannedString {
-            val parts = movie.titleHighlight?.parts
-
-            parts?.forEach { (part, isHighlighted) ->
-                if (isHighlighted) {
-                    println("High part: $part")
-                    bold { append(part) }
-                } else {
-                    println("Normal part: $part")
-                    append(part)
-                }
-            } ?: append(movie.title)
-            append("(${movie.year})")
-        }
-
-        //TODO: Move to own demo, not paging's
         //FIXME: Is there a way we can still use String.format?
         //view.itemTitle.text = view.resources.getString(R.string.template_title).format(title, movie.year)
-        view.itemTitle.text = title
-        view.itemSubtitle.text = movie.genre.sorted().joinToString { it }
+
+        view.itemTitle.text = TextUtils.concat(movie.highlightedTitle.toSpannedString(), " (${movie.year})")
+        val genreStrings = movie.highlightedGenres.sorted()
+            .map { it.toSpannedString() }
+        if (genreStrings.isNotEmpty()) {
+            view.itemSubtitle.text = genreStrings.reduce { acc, str ->
+                TextUtils.concat(acc, ", ", str) as SpannedString
+            }.also {
+                println("Final string: $it, spans: ${it.getSpans<StyleSpan>(0, it.length).size}")
+            }
+        }
         Glide.with(view)
             .load(movie.image).placeholder(android.R.drawable.ic_media_play)
             .centerCrop()
