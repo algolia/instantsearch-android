d7a78f3084b038c7866cdbc340b2a75f7b62bfdb
Paul-Louis NECH <pln@algolia.com>
2019-06-28 11:44:59 +0200

    chore(MovieViewHolder): Placeholder for missing genre

--- demo/src/main/kotlin/com/algolia/instantsearch/demo/list/movie/MovieViewHolder.kt
+++ demo/src/main/kotlin/com/algolia/instantsearch/demo/list/movie/MovieViewHolder.kt
@@ -2,6 +2,8 @@ package com.algolia.instantsearch.demo.list.movie
 
 import android.text.TextUtils
 import android.view.View
+import androidx.core.text.buildSpannedString
+import androidx.core.text.italic
 import androidx.recyclerview.widget.RecyclerView
 import com.algolia.instantsearch.helper.highlighting.toSpannedString
 import com.bumptech.glide.Glide
@@ -11,11 +13,9 @@ import kotlinx.android.synthetic.main.list_item_large.view.*
 class MovieViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
     fun bind(movie: Movie) {
-        //FIXME: Is there a way we can still use String.format?
-        //view.itemTitle.text = view.resources.getString(R.string.template_title).format(title, movie.year)
-
-        view.itemTitle.text = TextUtils.concat(movie.highlightedTitle.toSpannedString(), " (${movie.year})")
+        view.itemTitle.text = TextUtils.concat(movie.highlightedTitle?.toSpannedString(), " (${movie.year})")
         view.itemSubtitle.text = movie.highlightedGenres?.toSpannedString()
+            ?: buildSpannedString { italic { append("unknown genre") } }
         Glide.with(view)
             .load(movie.image).placeholder(android.R.drawable.ic_media_play)
             .centerCrop()
