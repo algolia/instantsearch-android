b7783d2ec36c0d26ff24ea6e0bc934926751e8e0
Paul-Louis NECH <pln@algolia.com>
2019-06-28 15:03:44 +0200

    refactor(highlighting.Extensions): Allow any Span

--- helper/src/androidMain/kotlin/com/algolia/instantsearch/helper/highlighting/Extensions.kt
+++ helper/src/androidMain/kotlin/com/algolia/instantsearch/helper/highlighting/Extensions.kt
@@ -1,21 +1,28 @@
 package com.algolia.instantsearch.helper.highlighting
 
+import android.graphics.Typeface
+import android.text.ParcelableSpan
 import android.text.SpannedString
 import android.text.TextUtils
-import androidx.core.text.bold
+import android.text.style.StyleSpan
 import androidx.core.text.buildSpannedString
+import androidx.core.text.inSpans
 import com.algolia.instantsearch.core.highlighting.HighlightedString
 
-public fun HighlightedString.toSpannedString(): SpannedString = buildSpannedString {
+public fun HighlightedString.toSpannedString(
+    span: ParcelableSpan = StyleSpan(Typeface.BOLD)
+): SpannedString = buildSpannedString {
     parts.forEach { (part, isHighlighted) ->
-        if (isHighlighted) bold { append(part) }
+        if (isHighlighted) inSpans(span) { append(part) }
         else append(part)
     }
 }
 
-public fun List<HighlightedString>.toSpannedString(): SpannedString {
+public fun List<HighlightedString>.toSpannedString(
+    span: ParcelableSpan = StyleSpan(Typeface.BOLD)
+): SpannedString {
     return buildSpannedString {
-        this@toSpannedString.map { it.toSpannedString() }.let {
+        this@toSpannedString.map { it.toSpannedString(span) }.let {
             if (it.isNotEmpty()) append(it.reduce { acc, str ->
                 TextUtils.concat(acc, ", ", str) as SpannedString
             })
