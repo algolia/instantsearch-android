05a2e8f605a482564a0feaf039ffdede30a3ef94
Paul-Louis NECH <pln@algolia.com>
2019-07-03 12:19:11 +0200

    refactor(HierarchicalItem): current -> displayName

--- demo/src/main/kotlin/com/algolia/instantsearch/demo/hierarchical/HierarchicalViewHolder.kt
+++ demo/src/main/kotlin/com/algolia/instantsearch/demo/hierarchical/HierarchicalViewHolder.kt
@@ -10,7 +10,7 @@ class HierarchicalViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
     fun bind(item: HierarchicalItem, onClick: View.OnClickListener) {
         view.elevation = ((6 - item.level * 2) * view.context.dip(4)).toFloat()
-        view.selectableItemName.text = item.current
+        view.selectableItemName.text = item.displayName
         view.selectableItemSubtitle.text = item.facet.count.toString()
         view.selectableItemSubtitle.visibility = View.VISIBLE
         view.setOnClickListener(onClick)
--- helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/hierarchical/HierarchicalItem.kt
+++ helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/hierarchical/HierarchicalItem.kt
@@ -5,6 +5,6 @@ import com.algolia.search.model.search.Facet
 
 public data class HierarchicalItem(
     val facet: Facet,
-    val current: String,
+    val displayName: String,
     val level: Int
 )
