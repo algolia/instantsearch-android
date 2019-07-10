19c4dc7aa7f1db9f1914b574dbfa7e9abf914c4a
Paul-Louis NECH <pln@algolia.com>
2019-07-10 18:03:37 +0200

    feat(HierarchicalConnectView): setItem when connecting

--- helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/hierarchical/HierarchicalConnectView.kt
+++ helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/hierarchical/HierarchicalConnectView.kt
@@ -4,8 +4,11 @@ import com.algolia.instantsearch.core.tree.TreeView
 
 
 public fun <T> HierarchicalViewModel.connectView(view: TreeView<T>, presenter: HierarchicalPresenter<T>) {
-    onItemChanged += {
+    val setItem: (HierarchicalTree) -> Unit = {
         view.setItem(presenter(it))
     }
+
+    setItem(item)
+    onItemChanged += setItem
     view.onClick = { computeSelections(it) }
 }
