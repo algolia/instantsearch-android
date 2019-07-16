d83bcfe16dfb6de7634b35c179e9f9ad7742f824
Paul-Louis NECH <pln@algolia.com>
2019-07-16 17:55:15 +0200

    fix(hierarchical.CFS): Corrected test

--- helper/src/commonTest/kotlin/hierarchical/TestHierarchicalConnectFilterState.kt
+++ helper/src/commonTest/kotlin/hierarchical/TestHierarchicalConnectFilterState.kt
@@ -2,29 +2,37 @@ package hierarchical
 
 import com.algolia.instantsearch.core.tree.Node
 import com.algolia.instantsearch.core.tree.Tree
-import com.algolia.instantsearch.core.tree.TreeView
-import com.algolia.instantsearch.helper.hierarchical.HierarchicalItem
-import com.algolia.instantsearch.helper.hierarchical.HierarchicalPresenterImpl
+import com.algolia.instantsearch.helper.filter.state.FilterGroupID
+import com.algolia.instantsearch.helper.filter.state.FilterState
+import com.algolia.instantsearch.helper.filter.state.toFilter
 import com.algolia.instantsearch.helper.hierarchical.HierarchicalViewModel
-import com.algolia.instantsearch.helper.hierarchical.connectView
+import com.algolia.instantsearch.helper.hierarchical.connectFilterState
 import com.algolia.search.model.Attribute
 import com.algolia.search.model.search.Facet
-import shouldBeEmpty
 import shouldEqual
-import shouldNotBeNull
 import kotlin.test.Test
 
 
-class TestHierarchicalConnectView {
+class TestHierarchicalConnectFilterState {
 
-    private val selections = listOf("Shoes > Running")
     private val separator = " > "
-    private val presenter = HierarchicalPresenterImpl(separator)
+    private val category = Attribute("category")
+    private val categories = listOf(
+        Attribute("$category.lvl0"),
+        Attribute("$category.lvl1")
+    )
+    private val groupIDName = categories.first().raw
+    private val groupID = FilterGroupID(groupIDName)
 
     private val facetShoes = Facet("Shoes", 3)
     private val facetShoesRunning = Facet("Shoes > Running", 2)
     private val facetShoesCocktail = Facet("Shoes > Cocktail", 1)
     private val facetBags = Facet("Bags", 1)
+    private val filterShoes = facetShoes.toFilter(categories[0])
+    private val filterShoesRunning = facetShoesRunning.toFilter(categories[1])
+    private val filterBags = facetBags.toFilter(categories[0])
+
+    private val selection = facetShoesRunning.value
     private val tree: Tree<Facet> = Tree(
         mutableListOf(
             Node(facetBags),
@@ -36,63 +44,44 @@ class TestHierarchicalConnectView {
             )
         )
     )
+    private val expectedFilterState = FilterState(
+        mapOf(
+            groupID to setOf(
+                filterShoesRunning
+            )
+        )
+    ).apply { hierarchicalFilters = listOf(filterShoes, filterShoesRunning) }
 
-    private fun Facet.toDisplayName() = value.split(separator).last()
+    @Test
+    fun connectShouldSetHierarchicalAttributes() {
+        val viewModel = HierarchicalViewModel(categories, separator, tree)
+        val filterState = FilterState()
 
-    private val expectedItems = listOf(
-        HierarchicalItem(facetBags, facetBags.toDisplayName(), 0),
-        HierarchicalItem(facetShoes, facetShoes.toDisplayName(), 0),
-        HierarchicalItem(facetShoesCocktail, facetShoesCocktail.toDisplayName(), 1),
-        HierarchicalItem(facetShoesRunning, facetShoesRunning.toDisplayName(), 1)
-    )
-    private val expectedSelection = listOf(facetShoes.value, facetShoesRunning.value)
-    private val attributes = listOf(
-        Attribute("category.lvl0"),
-        Attribute("category.lvl1")
-    )
+        viewModel.connectFilterState(filterState, category, groupIDName)
+        filterState.hierarchicalAttributes shouldEqual viewModel.hierarchicalAttributes
+    }
 
-    private class MockTreeView : TreeView<List<HierarchicalItem>> {
-        internal var items: List<HierarchicalItem> = listOf()
+    @Test
+    fun onSelectionsComputedShouldUpdateFilterState() {
+        val viewModel = HierarchicalViewModel(categories, separator, tree)
+        val filterState = FilterState()
 
-        override var onClick: ((String) -> Unit)? = null
+        viewModel.connectFilterState(filterState, category, groupIDName)
+        viewModel.computeSelections(selection)
+        filterState.filters shouldEqual expectedFilterState.filters
+        filterState.hierarchicalFilters shouldEqual expectedFilterState.hierarchicalFilters
+    }
 
-        override fun setItem(item: List<HierarchicalItem>) {
-            items = item
+    @Test
+    fun onFilterStateChangedShouldUpdateSelections() {
+        val viewModel = HierarchicalViewModel(categories, separator, tree)
+        val filterState = FilterState()
+
+        viewModel.connectFilterState(filterState, category, groupIDName)
+        filterState.notify {
+            remove(groupID, filterShoesRunning)
+            add(groupID, filterBags)
         }
-    }
-
-    @Test
-    fun connectShouldCallSetItem() {
-        val view = MockTreeView()
-        val viewModel = HierarchicalViewModel(attributes, separator, tree)
-
-        viewModel.selections = selections
-        viewModel.connectView(view, presenter)
-        view.items shouldEqual expectedItems
-    }
-
-    @Test
-    fun onItemChangedShouldCallSetItem() {
-        val view = MockTreeView()
-        val viewModel = HierarchicalViewModel(attributes, separator)
-
-        viewModel.connectView(view, presenter)
-        viewModel.item = tree.copy(children = mutableListOf())
-        view.items shouldEqual listOf()
-    }
-
-    @Test
-    fun onClickShouldCallOnSelectionsComputed() {
-        val view = MockTreeView()
-        val viewModel = HierarchicalViewModel(attributes, separator)
-
-        viewModel.onSelectionsComputed += { list ->
-            viewModel.selections = list.map { it.second }
-        }
-        viewModel.connectView(view, presenter)
-        view.items.shouldBeEmpty()
-        view.onClick.shouldNotBeNull()
-        view.onClick!!(selections.first())
-        viewModel.selections shouldEqual expectedSelection
+        viewModel.selections shouldEqual listOf(facetBags.value)
     }
 }
