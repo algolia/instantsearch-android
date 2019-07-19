4b5ef55516ef6651f27ab78140f486183a2a1744
Quentin LITZLER <qlitzler@gmail.com>
2019-07-19 13:26:24 +0200

    Remove MapView

--- core/src/commonMain/kotlin/com/algolia/instantsearch/core/map/MapConnectView.kt
+++
@@ -1,9 +0,0 @@
-package com.algolia.instantsearch.core.map
-
-import com.algolia.instantsearch.core.item.connectView
-
-
-public fun <K, V> MapViewModel<K, V>.connectView(view: MapView<K, V>) {
-    map.connectView(view) { it }
-    view.onClick = (::remove)
-}
--- core/src/commonMain/kotlin/com/algolia/instantsearch/core/map/MapView.kt
+++
@@ -1,7 +0,0 @@
-package com.algolia.instantsearch.core.map
-
-import com.algolia.instantsearch.core.event.EventView
-import com.algolia.instantsearch.core.item.ItemView
-
-
-public interface MapView<K, V> : EventView<K>, ItemView<Map<K, V>>
