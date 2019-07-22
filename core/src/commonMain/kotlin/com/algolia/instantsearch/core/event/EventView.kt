5abf9988776f64a1e8551fa2ee76bafc5f0684a3
Quentin LITZLER <qlitzler@gmail.com>
2019-07-22 15:52:21 +0200

    Remove event and item

--- core/src/commonMain/kotlin/com/algolia/instantsearch/core/event/EventView.kt
+++
@@ -1,7 +0,0 @@
-package com.algolia.instantsearch.core.event
-
-
-public interface EventView<T> {
-
-    public var onClick: ((T) -> Unit)?
-}
--- core/src/commonMain/kotlin/com/algolia/instantsearch/core/event/EventViewModelImpl.kt
+++
@@ -1,7 +0,0 @@
-package com.algolia.instantsearch.core.event
-
-
-public open class EventViewModelImpl<T>: EventViewModel<T> {
-
-    override val onTriggered: MutableList<(T) -> Unit> = mutableListOf()
-}
