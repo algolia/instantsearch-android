abc213a8bae45edd7b1bdf75fdbdfe98d2c9f117
Quentin LITZLER <qlitzler@gmail.com>
2019-07-23 16:42:47 +0200

    Update Kotlin to 1.3.41

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,19 +13,18 @@ buildscript {
     }
     dependencies {
         classpath(dependency.script.AndroidTools())
-        classpath(kotlin("gradle-plugin", version = "1.3.40"))
-        classpath(kotlin("serialization",  version = "1.3.40"))
+        classpath(kotlin("gradle-plugin", version = "1.3.41"))
+        classpath(kotlin("serialization",  version = "1.3.41"))
         classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
     }
 }
 
 allprojects {
     repositories {
-        mavenLocal()
         google()
         jcenter()
         mavenCentral()
-//        maven { url = URI("https://dl.bintray.com/algolia/maven") }
+        maven { url = URI("https://dl.bintray.com/algolia/maven") }
         maven { url = URI("https://kotlin.bintray.com/kotlinx") }
         maven { url = URI("https://dl.bintray.com/kotlin/ktor") }
     }
