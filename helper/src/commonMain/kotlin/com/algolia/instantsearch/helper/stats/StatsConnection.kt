f1f77e4526c28ec0acf23387f17f8aa8df1d6aea
Paul-Louis NECH <pln@algolia.com>
2019-08-01 16:47:03 +0200

    feat(StatsViewModel): connectView with default StatsPresenterImpl

--- helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/stats/StatsConnection.kt
+++ helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/stats/StatsConnection.kt
@@ -4,6 +4,13 @@ import com.algolia.instantsearch.core.connection.Connection
 import com.algolia.instantsearch.helper.searcher.SearcherSingleIndex
 
 
+public fun StatsViewModel.connectView(
+    view: StatsView<String>,
+    presenter: StatsPresenter<String> = StatsPresenterImpl()
+) : Connection {
+    return StatsConnectionView<String>(this, view, presenter)
+}
+
 public fun <T> StatsViewModel.connectView(
     view: StatsView<T>,
     presenter: StatsPresenter<T>
