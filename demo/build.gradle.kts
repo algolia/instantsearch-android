3c2de78f00731cc162f245d7b875eafdc558f6c4
Quentin LITZLER <qlitzler@gmail.com>
2019-08-06 19:02:08 +0200

    Better exclude rule in packagingOptions

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -1,4 +1,3 @@
-import dependency.network.Ktor
 import dependency.test.AndroidTestRunner
 import dependency.test.Espresso
 import dependency.ui.*
@@ -35,21 +34,7 @@ android {
 
     packagingOptions {
         // TODO: Solve these "More than one file was found with OS independent path 'META-INF/*' in the lib via Proguard
-        exclude("META-INF/kotlinx-serialization-runtime.kotlin_module")
-        exclude("META-INF/kotlinx-coroutines-core.kotlin_module")
-        exclude("META-INF/ktor-http.kotlin_module")
-        exclude("META-INF/kotlinx-io.kotlin_module")
-        exclude("META-INF/atomicfu.kotlin_module")
-        exclude("META-INF/ktor-utils.kotlin_module")
-        exclude("META-INF/kotlinx-coroutines-io.kotlin_module")
-        exclude("META-INF/ktor-client-json.kotlin_module")
-        exclude("META-INF/ktor-client-json.kotlin_module")
-        exclude("META-INF/ktor-client-logging.kotlin_module")
-        exclude("META-INF/algoliasearch-client-kotlin.kotlin_module")
-        exclude("META-INF/ktor-client-core.kotlin_module")
-        exclude("META-INF/ktor-client-serialization.kotlin_module")
-        exclude("META-INF/ktor-http-cio.kotlin_module")
-        exclude("META-INF/kotlinx-serialization-runtime.kotlin_module")
+        exclude("META-INF/*.kotlin_module")
     }
     (kotlinOptions as KotlinJvmOptions).apply {
         jvmTarget = JavaVersion.VERSION_1_8.toString()
