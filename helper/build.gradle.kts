948af9b8613bf71089928774d39c03af5db0e8e3
Quentin LITZLER <qlitzler@gmail.com>
2019-08-22 17:32:13 +0200

    Remove mavenLocal()

--- helper/build.gradle.kts
+++ helper/build.gradle.kts
@@ -90,7 +90,7 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 api(project(":core"))
-                api(AlgoliaClient())
+                api(AlgoliaClient("common"))
                 api(kotlin("stdlib-common"))
             }
         }
@@ -99,7 +99,7 @@ kotlin {
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
                 implementation(Ktor("client-mock"))
-                implementation(AlgoliaClient())
+                implementation(AlgoliaClient("common"))
             }
         }
         val jvmMain by getting {
