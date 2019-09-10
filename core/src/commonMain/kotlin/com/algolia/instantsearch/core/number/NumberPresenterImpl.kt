af434f4c6686152d64e77fa923f5d513581d2453
Paul-Louis NECH <pln@algolia.com>
2019-09-10 15:09:30 +0200

    refactor(NumberPresenter): Java DX, kdoc

--- core/src/commonMain/kotlin/com/algolia/instantsearch/core/number/NumberPresenterImpl.kt
+++ core/src/commonMain/kotlin/com/algolia/instantsearch/core/number/NumberPresenterImpl.kt
@@ -3,7 +3,16 @@ package com.algolia.instantsearch.core.number
 
 public object NumberPresenterImpl : NumberPresenter<Number> {
 
-    override fun invoke(number: Number?): String {
+    override fun invoke(number: Number?): String = present(number)
+
+    /**
+     * Presents the given number, transforming it into a [String].
+     *
+     * @param number the value to present.
+     *
+     * @return the number's string representation, or `"-"` if `null`.
+     */
+    fun present(number: Number?): String {
         return number?.toString() ?: "-"
     }
 }
