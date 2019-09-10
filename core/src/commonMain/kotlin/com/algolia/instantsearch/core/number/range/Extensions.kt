8d10415b5731aafcf967180b282ef4730adbe363
Paul-Louis NECH <pln@algolia.com>
2019-09-10 15:09:42 +0200

    chore(range): kdoc

--- core/src/commonMain/kotlin/com/algolia/instantsearch/core/number/range/Extensions.kt
+++ core/src/commonMain/kotlin/com/algolia/instantsearch/core/number/range/Extensions.kt
@@ -1,7 +1,12 @@
 package com.algolia.instantsearch.core.number.range
 
-
-internal fun <T> Range<T>.coerce(bounds: Range<T>?): Range<T> where T: Number, T: Comparable<T> {
+/**
+ * Coerces a Range into another, ensuring its min and max fit within the other Range's.
+ *
+ * @param bounds the Range to coerce this Range into.
+ * @return this if it was within the given [bounds], else a new Range coerced within.
+ */
+internal fun <T> Range<T>.coerce(bounds: Range<T>?): Range<T> where T : Number, T : Comparable<T> {
     return bounds?.let {
         val coercedMin = min.coerceIn(it.min, it.max)
         val coercedMax = max.coerceIn(it.min, it.max)
@@ -10,6 +15,6 @@ internal fun <T> Range<T>.coerce(bounds: Range<T>?): Range<T> where T: Number, T
     } ?: this
 }
 
-internal fun <T> T.coerce(bounds: Range<T>?): T where T: Number, T: Comparable<T> {
+internal fun <T> T.coerce(bounds: Range<T>?): T where T : Number, T : Comparable<T> {
     return bounds?.let { coerceIn(it.min, it.max) } ?: this
 }
