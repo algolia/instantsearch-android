cb886a0e4172d0428f214e81c7b7f9796ca7a421
Quentin LITZLER <qlitzler@gmail.com>
2019-09-17 11:07:26 +0200

    Add SearcherMultipleIndex connectFilterState

--- helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/searcher/SearcherConnection.kt
+++ helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/searcher/SearcherConnection.kt
@@ -4,6 +4,7 @@ import com.algolia.instantsearch.core.connection.Connection
 import com.algolia.instantsearch.core.searcher.Debouncer
 import com.algolia.instantsearch.core.searcher.debounceFilteringInMillis
 import com.algolia.instantsearch.helper.filter.state.FilterState
+import com.algolia.search.model.IndexName
 
 
 public fun SearcherSingleIndex.connectFilterState(
@@ -18,4 +19,12 @@ public fun SearcherForFacets.connectFilterState(
     debouncer: Debouncer = Debouncer(debounceFilteringInMillis)
 ): Connection {
     return SearcherForFacetsConnectionFilterState(this, filterState, debouncer)
+}
+
+public fun SearcherMultipleIndex.connectFilterState(
+    filterState: FilterState,
+    indexName: IndexName,
+    debouncer: Debouncer = Debouncer(debounceFilteringInMillis)
+): Connection {
+    return SearcherMultipleConnectionFilterState(this, filterState, indexName, debouncer)
 }
