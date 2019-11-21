54eccee56a3505ae38ee223dd88c4c4f73cabf05
Quentin LITZLER <qlitzler@gmail.com>
2019-11-21 16:30:46 +0100

    Add SearchBoxAutoCompleteTextView

---
+++ helper/src/androidMain/kotlin/com/algolia/instantsearch/helper/android/searchbox/SearchBoxAutoCompleteTextView.kt
@@ -0,0 +1,34 @@
+package com.algolia.instantsearch.helper.android.searchbox
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.AutoCompleteTextView
+import com.algolia.instantsearch.core.Callback
+import com.algolia.instantsearch.core.searchbox.SearchBoxView
+
+
+public class SearchBoxAutoCompleteTextView(
+    public val autoCompleteTextView: AutoCompleteTextView
+) : SearchBoxView {
+
+    override var onQueryChanged: Callback<String?>? = null
+    override var onQuerySubmitted: Callback<String?>? = null
+
+    init {
+        autoCompleteTextView.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable?) = Unit
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                onQueryChanged?.invoke(s?.toString())
+            }
+        })
+    }
+
+    override fun setText(text: String?, submitQuery: Boolean) {
+        autoCompleteTextView.setText(text)
+        if (submitQuery) onQuerySubmitted?.invoke(text)
+    }
+}
