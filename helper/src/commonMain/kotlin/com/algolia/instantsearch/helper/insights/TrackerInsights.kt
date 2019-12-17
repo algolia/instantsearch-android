e8c0092f2d6066dcb8214874d83479f42c5bcc76
Quentin LITZLER <qlitzler@gmail.com>
2019-12-17 16:07:49 +0100

    Pair programming vlad 2

--- helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/insights/TrackerInsights.kt
+++ helper/src/commonMain/kotlin/com/algolia/instantsearch/helper/insights/TrackerInsights.kt
@@ -6,10 +6,7 @@ import com.algolia.instantsearch.helper.searcher.SearcherSingleIndex
 import com.algolia.search.client.ClientInsights
 import com.algolia.search.client.ClientSearch
 import com.algolia.search.endpoint.EndpointInsightsUser
-import com.algolia.search.model.APIKey
-import com.algolia.search.model.ApplicationID
-import com.algolia.search.model.IndexName
-import com.algolia.search.model.ObjectID
+import com.algolia.search.model.*
 import com.algolia.search.model.filter.Filter
 import com.algolia.search.model.insights.EventName
 import com.algolia.search.model.insights.UserToken
@@ -17,45 +14,74 @@ import io.ktor.client.response.HttpResponse
 import kotlinx.coroutines.*
 
 
-fun stuff() {
-    val appID = ApplicationID("")
-    val apiKey = APIKey("")
-    val client = ClientSearch(appID, apiKey)
-    val indexName = IndexName("blabla")
-    val index = client.initIndex(indexName)
-    val searcher = SearcherSingleIndex(index)
-    val insights = ClientInsights(appID, apiKey)
-    val tracker = TrackerInsights(searcher, insights, UserToken("hello"))
-
-    tracker.view()
-}
-
 public class TrackerInsights(
-    val searcher: SearcherSingleIndex,
-    val clientInsights: ClientInsights,
-    val userToken: UserToken,
-    val coroutineScope: CoroutineScope = SearcherScope()
+    private val searcher: SearcherSingleIndex,
+    private val clientInsights: ClientInsights,
+    private val userToken: UserToken,
+    private val coroutineScope: CoroutineScope = SearcherScope()
 ) : EndpointInsightsUser by clientInsights.User(userToken) {
 
     val error = SubscriptionValue<Throwable?>(null)
     val response = SubscriptionValue<HttpResponse?>(null)
 
     private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        searcher.error.value = throwable
+        error.value = throwable
     }
 
-    fun clickedObjectIDs(eventName: EventName, objectIDs: List<ObjectID>) {
+    public fun clickedObjectID(eventName: EventName, objectIDs: List<ObjectID>, positions: List<Int>) {
         coroutineScope.launch(exceptionHandler) {
             response.value = withContext(Dispatchers.Default) {
-                clickedObjectIDs(searcher.index.indexName, eventName, objectIDs)
+                val currentQueryID = searcher.response.value?.queryID
+
+                if (currentQueryID != null) {
+                    clickedObjectIDsAfterSearch(searcher.index.indexName, eventName, currentQueryID, objectIDs, positions)
+                } else {
+                    clickedObjectIDs(searcher.index.indexName, eventName, objectIDs)
+                }
             }
         }
     }
 
-    fun clickedFilters(eventName: EventName, filters: List<Filter.Facet>) {
+    public fun clickedFilter(eventName: EventName, filters: List<Filter.Facet>) {
         coroutineScope.launch(exceptionHandler) {
             response.value = withContext(Dispatchers.Default) {
                 clickedFilters(searcher.index.indexName, eventName, filters)
+            }
+        }
+    }
+
+    public fun viewedObjectID(eventName: EventName, objectID: ObjectID) {
+        coroutineScope.launch(exceptionHandler) {
+            response.value = withContext(Dispatchers.Default) {
+                viewedObjectIDs(searcher.index.indexName, eventName, listOf(objectID))
+            }
+        }
+    }
+
+    public fun viewedFilters(eventName: EventName, filters: List<Filter.Facet>) {
+        coroutineScope.launch(exceptionHandler) {
+            response.value = withContext(Dispatchers.Default) {
+                viewedFilters(searcher.index.indexName, eventName, filters)
+            }
+        }
+    }
+
+    public fun convertedObjectIDs(eventName: EventName, objectIDs: List<ObjectID>) {
+        coroutineScope.launch(exceptionHandler) {
+            response.value = withContext(Dispatchers.Default) {
+                val currentQueryID = searcher.response.value?.queryID
+
+                if (currentQueryID != null) {
+                    convertedObjectIDsAfterSearch(searcher.index.indexName, eventName, currentQueryID, objectIDs)
+                } else convertedObjectIDs(searcher.index.indexName, eventName, objectIDs)
+            }
+        }
+    }
+
+    public fun convertedFilters(eventName: EventName, filters: List<Filter.Facet>) {
+        coroutineScope.launch(exceptionHandler) {
+            response.value = withContext(Dispatchers.Default) {
+                convertedFilters(searcher.index.indexName, eventName, filters)
             }
         }
     }
