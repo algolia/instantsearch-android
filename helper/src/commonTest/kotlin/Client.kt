561259d56599e8564b0cbdc525ca6f95b800d98e
Guy Daher <guydaher1@gmail.com>
2020-02-24 15:09:21 +0100

    Release of 2.3.0:
    - Use Kotlin client `1.3.1`
    - Updated Ktor to `1.3.0`

--- helper/src/commonTest/kotlin/Client.kt
+++ helper/src/commonTest/kotlin/Client.kt
@@ -3,13 +3,15 @@ import com.algolia.search.configuration.ConfigurationSearch
 import com.algolia.search.model.APIKey
 import com.algolia.search.model.ApplicationID
 import com.algolia.search.model.response.ResponseSearch
+import com.algolia.search.model.response.ResponseSearchForFacets
 import io.ktor.client.engine.mock.MockEngine
 import io.ktor.client.engine.mock.respond
+import io.ktor.client.engine.mock.respondBadRequest
 import io.ktor.client.features.logging.LogLevel
 import io.ktor.client.request.HttpResponseData
 import io.ktor.http.ContentType
 import io.ktor.http.headersOf
-import kotlinx.coroutines.io.ByteReadChannel
+import io.ktor.utils.io.ByteReadChannel
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
@@ -17,26 +19,57 @@ import kotlinx.serialization.json.JsonConfiguration
 val JsonNoDefaults = Json(JsonConfiguration.Stable.copy(encodeDefaults = false))
 
 fun mockClient(
-    response: HttpResponseData = respondSearch()
+    response: HttpResponseData? = null
 ): ClientSearch {
+    val mockEngine = if (response != null) MockEngine { response } else { defaultMockEngine }
+    return mockClient(mockEngine)
+}
+
+fun mockClient(
+    mockEngine: MockEngine): ClientSearch {
     return ClientSearch(
         ConfigurationSearch(
             ApplicationID("A"),
             APIKey("B"),
-            engine = MockEngine { response },
+            engine = mockEngine,
             logLevel = LogLevel.ALL
         )
     )
 }
 
-val responseSearch = ResponseSearch()
-
 fun respondSearch(response: ResponseSearch = responseSearch) = respondJson(response, ResponseSearch.serializer())
 
-fun <T> respondJson(response: T, serializer: KSerializer<T>): HttpResponseData {
+fun <T> respondJson(response: T, serializer: KSerializer<T>): MockEngine {
     val responseString = JsonNoDefaults.stringify(serializer, response)
+    return MockEngine {
+        respond(
+            headers = headersOf(
+                "Content-Type",
+                listOf(ContentType.Application.Json.toString())
+            ),
+            content = ByteReadChannel(responseString)
+        )
+    }
+}
 
-    return respond(
+fun respondBadRequest(): ClientSearch {
+    val mockEngine = MockEngine {
+        respondBadRequest()
+    }
+    return ClientSearch(
+        ConfigurationSearch(
+            ApplicationID("A"),
+            APIKey("B"),
+            engine = mockEngine,
+            logLevel = LogLevel.ALL
+        )
+    )
+}
+
+val defaultMockEngine = MockEngine {
+    val responseString = JsonNoDefaults.stringify(ResponseSearch.serializer(), responseSearch)
+
+    respond(
         headers = headersOf(
             "Content-Type",
             listOf(ContentType.Application.Json.toString())
@@ -44,3 +77,5 @@ fun <T> respondJson(response: T, serializer: KSerializer<T>): HttpResponseData {
         content = ByteReadChannel(responseString)
     )
 }
+
+val responseSearch = ResponseSearch()
