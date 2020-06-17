af64785396265bc15e06164ab3a0cc6c077f3ee9
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-06-17 14:42:07 +0200

    feat(insights): temporary java logging

--- insights/src/jvmMain/kotlin/com/algolia/instantsearch/insights/internal/Log.kt
+++ insights/src/jvmMain/kotlin/com/algolia/instantsearch/insights/internal/Log.kt
@@ -1,7 +1,10 @@
 package com.algolia.instantsearch.insights.internal
 
 actual class Log {
+
     actual companion object {
+
+        @JvmStatic
         actual fun d(tag: String?, msg: String) {
             val tagged = tag?.let { "[$it] " }
             println("$tagged$msg")
