e1538c9caa7a25daf5c555fa854ac5359350415a
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-08-19 18:39:46 +0200

    setup HMPP

--- helper/src/jvmTest/kotlin/Actual.kt
+++
@@ -1,7 +0,0 @@
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.runBlocking
-
-
-internal actual fun blocking(block: suspend CoroutineScope.() -> Unit) {
-    runBlocking(block = block)
-}
