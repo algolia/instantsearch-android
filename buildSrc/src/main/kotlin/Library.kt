077c0afb3d5b9f80239429c694d2261f61467648
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-08-20 14:58:17 +0200

    update ktor 1.4.0

--- buildSrc/src/main/kotlin/Library.kt
+++ buildSrc/src/main/kotlin/Library.kt
@@ -4,16 +4,8 @@ object Library: Dependency {
 
     override val group = "com.algolia"
     override val artifact = "instantsearch"
-    override val version = "2.5.1"
+    override val version = "3.0.0-alpha01"
 
-    val packageName = "$group:$artifact-android"
-
-    val artifactCore = "$artifact-core"
-    val artifactCoreCommon = "$artifactCore-common"
-    val artifactCoreJvm = "$artifactCore-jvm"
-
-    val artifactHelper = "$artifact-helper"
-    val artifactHelperCommon = "$artifactHelper-common"
-    val artifactHelperJvm = "$artifactHelper-jvm"
-    val artifactHelperAndroid = "$artifact-android"
+    val androidArtifact = "$artifact-android"
+    val packageName = "$group:$androidArtifact"
 }
