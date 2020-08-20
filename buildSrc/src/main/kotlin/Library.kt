57d461d8d60e09068954cf95807a15ed0ebf740c
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-08-20 17:34:11 +0200

    Prepare 2.6.0-alpha01

--- buildSrc/src/main/kotlin/Library.kt
+++ buildSrc/src/main/kotlin/Library.kt
@@ -4,7 +4,7 @@ object Library: Dependency {
 
     override val group = "com.algolia"
     override val artifact = "instantsearch"
-    override val version = "3.0.0-alpha01"
+    override val version = "2.6.0-alpha01"
 
     val androidArtifact = "$artifact-android"
     val packageName = "$group:$androidArtifact"
