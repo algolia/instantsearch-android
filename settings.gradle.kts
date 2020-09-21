4c66bb395dbfe9088345e73bd4fa12221d666de5
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-09-21 10:37:34 +0200

    chore: remove unneeded pluginManagement block

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,3 @@
-pluginManagement {
-    repositories {
-        mavenCentral()
-        google()
-        jcenter()
-        maven("https://plugins.gradle.org/m2/")
-    }
-}
 rootProject.name = "instantsearch-android"
 
 include(":instantsearch-android-core")
