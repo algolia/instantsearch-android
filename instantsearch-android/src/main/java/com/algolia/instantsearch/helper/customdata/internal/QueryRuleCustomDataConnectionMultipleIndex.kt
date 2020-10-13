c2a777032f997809399949d66dc9c9935aa55861
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-10-13 15:47:27 +0200

    feat(queryRuleCustomData): implement the widget

---
+++ instantsearch-android/src/main/java/com/algolia/instantsearch/helper/customdata/internal/QueryRuleCustomDataConnectionMultipleIndex.kt
@@ -0,0 +1,35 @@
+package com.algolia.instantsearch.helper.customdata.internal
+
+import com.algolia.instantsearch.core.Callback
+import com.algolia.instantsearch.core.connection.ConnectionImpl
+import com.algolia.instantsearch.helper.customdata.QueryRuleCustomDataViewModel
+import com.algolia.instantsearch.helper.searcher.SearcherMultipleIndex
+import com.algolia.search.model.response.ResponseSearches
+
+/**
+ * Connection between a rule custom data logic and a single index searcher.
+ *
+ * @param searcher searcher that handles your searches
+ * @param viewModel logic applied to the custom model
+ * @param queryIndex index of query from response of which the user data will be extracted
+ */
+internal class QueryRuleCustomDataConnectionMultipleIndex<T>(
+    val viewModel: QueryRuleCustomDataViewModel<T>,
+    val searcher: SearcherMultipleIndex,
+    private val queryIndex: Int,
+) : ConnectionImpl() {
+
+    private val callback: Callback<ResponseSearches?> = { responses ->
+        responses?.results?.getOrNull(queryIndex)?.let { viewModel.extractModel(it) }
+    }
+
+    override fun connect() {
+        super.connect()
+        searcher.response.subscribe(callback)
+    }
+
+    override fun disconnect() {
+        super.disconnect()
+        searcher.response.unsubscribe(callback)
+    }
+}
---
+++ instantsearch-android/src/main/java/com/algolia/instantsearch/helper/customdata/internal/QueryRuleCustomDataConnectionSingleIndex.kt
@@ -0,0 +1,33 @@
+package com.algolia.instantsearch.helper.customdata.internal
+
+import com.algolia.instantsearch.core.Callback
+import com.algolia.instantsearch.core.connection.ConnectionImpl
+import com.algolia.instantsearch.helper.customdata.QueryRuleCustomDataViewModel
+import com.algolia.instantsearch.helper.searcher.SearcherSingleIndex
+import com.algolia.search.model.response.ResponseSearch
+
+/**
+ * Connection between a rule custom data logic and a single index searcher.
+ *
+ * @param searcher searcher that handles your searches
+ * @param viewModel logic applied to the custom model
+ */
+internal class QueryRuleCustomDataConnectionSingleIndex<T>(
+    val viewModel: QueryRuleCustomDataViewModel<T>,
+    val searcher: SearcherSingleIndex,
+) : ConnectionImpl() {
+
+    private val callback: Callback<ResponseSearch?> = { response ->
+        response?.let { viewModel.extractModel(it) }
+    }
+
+    override fun connect() {
+        super.connect()
+        searcher.response.subscribe(callback)
+    }
+
+    override fun disconnect() {
+        super.disconnect()
+        searcher.response.unsubscribe(callback)
+    }
+}
