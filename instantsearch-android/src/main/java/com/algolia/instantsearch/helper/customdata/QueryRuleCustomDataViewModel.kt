20734d6ddb542c25f558751fcbbfbf57203a57f7
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-10-14 11:06:49 +0200

    feat(queryRuleCustomData): add convenience function builders

--- instantsearch-android/src/main/java/com/algolia/instantsearch/helper/customdata/QueryRuleCustomDataViewModel.kt
+++ instantsearch-android/src/main/java/com/algolia/instantsearch/helper/customdata/QueryRuleCustomDataViewModel.kt
@@ -3,26 +3,26 @@ package com.algolia.instantsearch.helper.customdata
 import com.algolia.instantsearch.core.subscription.SubscriptionValue
 import com.algolia.instantsearch.helper.extension.tryOrNull
 import com.algolia.search.model.response.ResponseSearch
-import kotlinx.serialization.KSerializer
+import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.serializer
 
 /**
  * Component encapsulating the logic applied to the custom model
  *
- * @param item initial item
- * @param type model's type to be deserialized
+ * @param deserializer deserializes the model into a value of type T
+ * @param initialItem initial item
  */
 public class QueryRuleCustomDataViewModel<T>(
-    public val serializer: KSerializer<T>,
-    item: T? = null,
+    private val deserializer: DeserializationStrategy<T>,
+    initialItem: T? = null,
 ) {
 
-    public val item: SubscriptionValue<T?> = SubscriptionValue(item)
+    public val item: SubscriptionValue<T?> = SubscriptionValue(initialItem)
 
     public fun extractModel(responseSearch: ResponseSearch) {
         item.value = responseSearch.userDataOrNull?.asSequence()
-            ?.mapNotNull { tryOrNull<T> { Json.decodeFromJsonElement(serializer, it) } }
+            ?.mapNotNull { tryOrNull<T> { Json.decodeFromJsonElement(deserializer, it) } }
             ?.firstOrNull()
     }
 }
