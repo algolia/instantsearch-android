37d8d5b55bc72c8bea675b690e4e583f050970f9
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-10-16 11:34:21 +0200

    feat(queryRuleCustomData): add more convenience function builders

--- instantsearch-android/src/test/java/customdata/TestQueryRuleCustomData.kt
+++ instantsearch-android/src/test/java/customdata/TestQueryRuleCustomData.kt
@@ -1,5 +1,6 @@
 package customdata
 
+import com.algolia.instantsearch.helper.customdata.QueryRuleCustomDataConnector
 import com.algolia.instantsearch.helper.customdata.QueryRuleCustomDataViewModel
 import com.algolia.instantsearch.helper.customdata.connectSearcher
 import com.algolia.instantsearch.helper.searcher.SearcherMultipleIndex
@@ -58,5 +59,83 @@ class TestQueryRuleCustomData {
         )
 
         viewModel.item.value shouldEqual customData2
+    }
+
+    @Test
+    fun testFunctionBuildersSingleIndex() {
+        val index = client.initIndex(IndexName("A"))
+        val searcher = SearcherSingleIndex(index)
+        val initialModel = TestModel(number = 10, text = "test1")
+
+        // explicit
+        QueryRuleCustomDataConnector(searcher = searcher,
+            deserializer = TestModel.serializer(),
+            initialItem = initialModel) {
+            it shouldEqual initialModel
+        }
+
+        // minimal
+        QueryRuleCustomDataConnector<TestModel>(searcher = searcher) {
+            it shouldEqual initialModel
+        }
+
+        // w/ initial
+        QueryRuleCustomDataConnector(searcher = searcher, initialItem = initialModel) {
+            it shouldEqual initialModel
+        }
+
+        // without initial
+        QueryRuleCustomDataConnector<TestModel>(searcher = searcher) {
+            it shouldEqual initialModel
+        }
+
+        // w/ ViewModel
+        QueryRuleCustomDataConnector(searcher = searcher,
+            viewModel = QueryRuleCustomDataViewModel(initialItem = initialModel)) {
+            it shouldEqual initialModel
+        }
+    }
+
+    @Test
+    fun testFunctionBuildersMultipleIndex() {
+        val indexA = IndexQuery(IndexName("IndexMovie"))
+        val indexB = IndexQuery(IndexName("IndexActor"))
+        val searcher = SearcherMultipleIndex(client, listOf(indexA, indexB))
+        val queryIndex = 1
+        val initialModel = TestModel(number = 10, text = "test1")
+
+        // explicit
+        QueryRuleCustomDataConnector(
+            searcher = searcher,
+            queryIndex = queryIndex,
+            deserializer = TestModel.serializer(),
+            initialItem = initialModel
+        ) {
+            it shouldEqual initialModel
+        }
+
+        // minimal
+        QueryRuleCustomDataConnector<TestModel>(searcher = searcher, queryIndex = queryIndex) {
+            it shouldEqual initialModel
+        }
+
+        // w/ initial
+        QueryRuleCustomDataConnector(searcher = searcher, queryIndex = queryIndex, initialItem = initialModel) {
+            it shouldEqual initialModel
+        }
+
+        // without initial
+        QueryRuleCustomDataConnector<TestModel>(searcher = searcher, queryIndex = queryIndex) {
+            it shouldEqual initialModel
+        }
+
+        // w/ ViewModel
+        QueryRuleCustomDataConnector(
+            searcher = searcher,
+            queryIndex = queryIndex,
+            viewModel = QueryRuleCustomDataViewModel(initialItem = initialModel)
+        ) {
+            it shouldEqual initialModel
+        }
     }
 }
