f10a39f8b0a27294fa9320da8346ba713100acd5
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-10-19 17:44:20 +0200

    feat: add SubscriptionValue's flow and livedata extensions

---
+++ instantsearch-android-ext/src/main/java/com/algolia/instantsearch/extension/internal/Flow.kt
@@ -0,0 +1,10 @@
+package com.algolia.instantsearch.extension.internal
+
+import kotlinx.coroutines.channels.SendChannel
+
+/**
+ * [SendChannel.offer] that returns `false` when this [SendChannel.isClosedForSend], instead of throwing.
+ */
+internal fun <E> SendChannel<E>.offerCatching(element: E): Boolean {
+    return runCatching { offer(element) }.getOrDefault(false)
+}
