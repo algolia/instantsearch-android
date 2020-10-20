dec9c3d49ecc01e697b6373b44dbf461065c9f5b
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-10-20 16:17:33 +0200

    feat: add flow and livedata extensions to SubscriptionEvent

---
+++ instantsearch-android-ext/src/main/java/com/algolia/instantsearch/extension/subscription/SubscriptionEvent.kt
@@ -0,0 +1,37 @@
+package com.algolia.instantsearch.extension.subscription
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import com.algolia.instantsearch.core.subscription.SubscriptionEvent
+import com.algolia.instantsearch.extension.internal.offerCatching
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+/**
+ * Convert SubscriptionEvent to Flow.
+ */
+@OptIn(ExperimentalCoroutinesApi::class)
+public fun <T> SubscriptionEvent<T>.asFlow(): Flow<T> {
+    return callbackFlow {
+        val subscription: (T) -> Unit = { value: T ->
+            offerCatching(value)
+        }
+        subscribe(subscription)
+        awaitClose { unsubscribe(subscription) }
+    }
+}
+
+/**
+ * SubscriptionEvent to LiveData.
+ *
+ * @param context The CoroutineContext to collect the upstream flow in.
+ * @param timeoutInMs The timeout in ms before cancelling the block if there are no active observers.
+ */
+public fun <T> SubscriptionEvent<T>.asLiveData(
+    context: CoroutineContext = EmptyCoroutineContext,
+    timeoutInMs: Long = 5000L,
+): LiveData<T> = asFlow().asLiveData(context, timeoutInMs)
---
+++ instantsearch-android-ext/src/test/java/com/algolia/instantsearch/extension/subscription/subscription/SubscriptionEventTest.kt
@@ -0,0 +1,61 @@
+package com.algolia.instantsearch.extension.subscription.subscription
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.algolia.instantsearch.core.subscription.SubscriptionEvent
+import com.algolia.instantsearch.core.subscription.SubscriptionValue
+import com.algolia.instantsearch.extension.subscription.MainCoroutineRule
+import com.algolia.instantsearch.extension.subscription.asFlow
+import com.algolia.instantsearch.extension.subscription.asLiveData
+import com.algolia.instantsearch.extension.subscription.runBlocking
+import com.algolia.instantsearch.extension.subscription.testCoroutineScope
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import org.junit.Rule
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class SubscriptionEventTest {
+
+    @get:Rule
+    val coroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    val testInstantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun testAsFlow() {
+        coroutineRule.runBlocking {
+            val subscriptionEvent = SubscriptionEvent<Int>()
+            val results = mutableListOf<Int>()
+            testCoroutineScope {
+                subscriptionEvent.asFlow()
+                    .onEach { results.add(it) }
+                    .launchIn(this)
+                subscriptionEvent.send(1)
+                subscriptionEvent.send(2)
+            }
+            subscriptionEvent.send(3)
+            assertEquals(2, results.size)
+        }
+    }
+
+    @Test
+    fun testLiveData() {
+        coroutineRule.runBlocking {
+            val subscriptionEvent = SubscriptionEvent<Int>()
+            val results = mutableListOf<Int>()
+            val observer = Observer<Int> { results.add(it) }
+
+            val livedata = subscriptionEvent.asLiveData()
+
+            livedata.observeForever(observer)
+            subscriptionEvent.send(1)
+            subscriptionEvent.send(2)
+            livedata.removeObserver(observer)
+            subscriptionEvent.send(3)
+
+            assertEquals(2, results.size)
+        }
+    }
+}
