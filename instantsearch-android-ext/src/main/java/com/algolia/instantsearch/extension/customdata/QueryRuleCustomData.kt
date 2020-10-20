84dbeab4d407d7fb6d48471c750adb6a9e94ce9b
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-10-20 15:14:10 +0200

    feat: add 'model' extension to QueryRuleCustomDataConnector

---
+++ instantsearch-android-ext/src/main/java/com/algolia/instantsearch/extension/customdata/QueryRuleCustomData.kt
@@ -0,0 +1,10 @@
+package com.algolia.instantsearch.extension.customdata
+
+import com.algolia.instantsearch.core.subscription.SubscriptionValue
+import com.algolia.instantsearch.helper.customdata.QueryRuleCustomDataConnector
+
+/**
+ * Convenience field to access custom data as SubscriptionValue.
+ */
+public val <T> QueryRuleCustomDataConnector<T>.model: SubscriptionValue<T?>
+    get() = viewModel.item
