6ca604d8cd838920e3d90145474141b3f9af1508
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-10-20 16:22:02 +0200

    feat: add search box convenience 'events'

---
+++ instantsearch-android-ext/src/main/java/com/algolia/instantsearch/extension/searchbox/SearchBox.kt
@@ -0,0 +1,10 @@
+package com.algolia.instantsearch.extension.searchbox
+
+import com.algolia.instantsearch.core.subscription.SubscriptionEvent
+import com.algolia.instantsearch.helper.searchbox.SearchBoxConnector
+
+/**
+ * Convenience field to access custom data as SubscriptionValue.
+ */
+public val <T> SearchBoxConnector<T>.submits: SubscriptionEvent<String?>
+    get() = viewModel.eventSubmit
