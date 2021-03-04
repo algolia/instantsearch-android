c4edb1bbf9cba69a367e15c49e463a14facab9da
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-03-04 15:47:22 +0100

    fix(subscription): make subscriptions list thread-safe (#230)
    LinkedHashSet (from mutableSetOf) is not thread-safe. Modifying it while
    iterating over it causes a ConcurrentModificationException to be thrown.
    This can typically happen when subscribing/unsubscribing to a
    Subscription instance while notifying all subscriptions.
    
    CopyOnWriteArraySet is useful for the case where the number of elements
    is generally small and read-only operations vastly outnumber mutative
    operations. This seems to match our use case here.

--- instantsearch-android-core/src/main/kotlin/com/algolia/instantsearch/core/subscription/SubscriptionEvent.kt
+++ instantsearch-android-core/src/main/kotlin/com/algolia/instantsearch/core/subscription/SubscriptionEvent.kt
@@ -3,6 +3,6 @@ package com.algolia.instantsearch.core.subscription
 public class SubscriptionEvent<T> : Subscription<T>() {
 
     public fun send(event: T) {
-        subscriptions.forEach { it(event) }
+        notifyAll(event)
     }
 }
