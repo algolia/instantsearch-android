f6c1fdb1e526c00beb6986ae267940b0bb611fba
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-05-28 10:17:32 +0200

    refact: facet list hoisted state

--- instantsearch-compose/src/main/java/com/algolia/instantsearch/compose/filter/FacetListCompose.kt
+++ instantsearch-compose/src/main/java/com/algolia/instantsearch/compose/filter/FacetListCompose.kt
@@ -1,8 +1,6 @@
 package com.algolia.instantsearch.compose.filter
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.Stable
 import com.algolia.instantsearch.compose.filter.internal.FacetListComposeImpl
 import com.algolia.instantsearch.core.selectable.list.SelectableItem
 import com.algolia.instantsearch.helper.filter.facet.FacetListView
@@ -11,21 +9,13 @@ import com.algolia.search.model.search.Facet
 /**
  * [FacetListView] for compose.
  */
+@Stable
 public interface FacetListCompose : FacetListView {
 
     /**
      * State holding selectable facets list value.
      */
-    public val facets: State<List<SelectableItem<Facet>>>
-}
-
-/**
- * Creates an instance of [FacetListCompose].
- *
- * @param facets state holding selectable facets list value
- */
-public fun FacetListCompose(facets: MutableState<List<SelectableItem<Facet>>>): FacetListCompose {
-    return FacetListComposeImpl(facets)
+    public var facets: List<SelectableItem<Facet>>
 }
 
 /**
@@ -34,5 +24,5 @@ public fun FacetListCompose(facets: MutableState<List<SelectableItem<Facet>>>): 
  * @param facets selectable facets list
  */
 public fun FacetListCompose(facets: List<SelectableItem<Facet>> = emptyList()): FacetListCompose {
-    return FacetListComposeImpl(mutableStateOf(facets))
+    return FacetListComposeImpl(facets)
 }
--- instantsearch-compose/src/main/java/com/algolia/instantsearch/compose/filter/internal/FacetListComposeImpl.kt
+++ instantsearch-compose/src/main/java/com/algolia/instantsearch/compose/filter/internal/FacetListComposeImpl.kt
@@ -1,6 +1,8 @@
 package com.algolia.instantsearch.compose.filter.internal
 
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.algolia.instantsearch.compose.filter.FacetListCompose
 import com.algolia.instantsearch.core.Callback
 import com.algolia.instantsearch.core.selectable.list.SelectableItem
@@ -12,12 +14,29 @@ import com.algolia.search.model.search.Facet
  * @param facets state holding selectable facets list value
  */
 internal class FacetListComposeImpl(
-    override val facets: MutableState<List<SelectableItem<Facet>>>
+    facets: List<SelectableItem<Facet>>
 ) : FacetListCompose {
+
+    override var facets by mutableStateOf(facets)
 
     override var onSelection: Callback<Facet>? = null
 
     override fun setItems(items: List<SelectableItem<Facet>>) {
-        this.facets.value = items
+        this.facets = items
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as FacetListComposeImpl
+        if (facets != other.facets) return false
+        if (onSelection != other.onSelection) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = facets.hashCode()
+        result = 31 * result + (onSelection?.hashCode() ?: 0)
+        return result
     }
 }
