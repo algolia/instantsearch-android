f9d3c402403208df3cfb29730d4806d8dd11486f
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-06-02 17:02:29 +0200

    feat: filter toggle widget

---
+++ instantsearch-compose/src/main/java/com/algolia/instantsearch/compose/filter/toggle/FilterToggleState.kt
@@ -0,0 +1,39 @@
+@file:Suppress("FunctionName")
+
+package com.algolia.instantsearch.compose.filter.toggle
+
+import com.algolia.instantsearch.compose.filter.toggle.internal.FilterToggleStateImpl
+import com.algolia.instantsearch.helper.filter.toggle.FilterToggleView
+
+/**
+ * [FilterToggleView] for compose.
+ */
+public interface FilterToggleState : FilterToggleView {
+
+    /**
+     * Text to be displayed.
+     */
+    public val text: String
+
+    /**
+     * The button state, True if checked, otherwise false.
+     */
+    public val isSelected: Boolean
+
+    /**
+     * Change the selected state.
+     *
+     * @param isSelected the new checked state of buttonView.
+     */
+    public fun onSelectionChanged(isSelected: Boolean)
+}
+
+/**
+ * Creates an instance of [FilterToggleState].
+ *
+ * @param text initial text value
+ * @param isSelected initial selection value
+ */
+public fun FilterToggleState(text: String, isSelected: Boolean = false): FilterToggleState {
+    return FilterToggleStateImpl(text, isSelected)
+}
