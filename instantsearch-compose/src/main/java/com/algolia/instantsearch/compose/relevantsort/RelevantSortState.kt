40da73bfe1b4659e27b296276e81130d401c9723
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-06-22 15:08:13 +0200

    relevant sort impl

---
+++ instantsearch-compose/src/main/java/com/algolia/instantsearch/compose/relevantsort/RelevantSortState.kt
@@ -0,0 +1,24 @@
+package com.algolia.instantsearch.compose.relevantsort
+
+import com.algolia.instantsearch.compose.relevantsort.internal.RelevantSortStateImpl
+import com.algolia.instantsearch.core.relevantsort.RelevantSortView
+
+/**
+ * [RelevantSortView] for compose.
+ */
+public interface RelevantSortState<T> : RelevantSortView<T> {
+
+    /**
+     * Current value of relevant sort.
+     */
+    public val value: T
+}
+
+/**
+ * Creates an instance of [RelevantSortState].
+ *
+ * @param value initial value
+ */
+public fun <T> RelevantSortState(value: T): RelevantSortState<T> {
+    return RelevantSortStateImpl(value)
+}
