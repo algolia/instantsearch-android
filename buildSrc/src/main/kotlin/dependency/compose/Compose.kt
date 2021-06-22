09016cc036dd0b78d0bfb094ec7471003ed02267
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-06-22 15:08:13 +0200

    relevant sort impl

--- buildSrc/src/main/kotlin/dependency/compose/Compose.kt
+++ buildSrc/src/main/kotlin/dependency/compose/Compose.kt
@@ -5,5 +5,9 @@ import dependency.Dependency
 interface Compose : Dependency {
 
     override val version: String
-        get() = "1.0.0-beta09"
+        get() = Compose.version
+
+    companion object {
+        const val version = "1.0.0-beta09"
+    }
 }
