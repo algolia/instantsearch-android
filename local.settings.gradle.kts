a98a315d5d82a3669087c76642e7a2fb1ffe5f37
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-10-15 18:55:42 +0200

    remove unneeded files

--- local.settings.gradle.kts
+++
@@ -1,6 +0,0 @@
-includeBuild("/Users/mouaad.aallam/Developer/Algolia/client/algoliasearch-client-kotlin") {
-    dependencySubstitution {
-        substitute(module("com.algolia:algoliasearch-client-kotlin"))
-            .with(project(":client"))
-    }
-}
