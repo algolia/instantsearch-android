745bf3a3afe68df8d29c4da481caec655e480f9c
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-10-18 17:36:39 +0200

    add disjunctive facting enabled search

--- instantsearch/src/commonMain/kotlin/com/algolia/instantsearch/helper/searcher/multi/hits/internal/HitsService.kt
+++ instantsearch/src/commonMain/kotlin/com/algolia/instantsearch/helper/searcher/multi/hits/internal/HitsService.kt
@@ -2,24 +2,32 @@ package com.algolia.instantsearch.helper.searcher.multi.hits.internal
 
 import com.algolia.instantsearch.helper.searcher.multi.internal.SearchService
 import com.algolia.search.client.ClientSearch
+import com.algolia.search.model.filter.FilterGroup
 import com.algolia.search.model.multipleindex.IndexQuery
 import com.algolia.search.model.response.ResponseSearch
 import com.algolia.search.transport.RequestOptions
 
 internal class HitsService(
     val client: ClientSearch
-) : SearchService<IndexQuery, ResponseSearch> {
+) : SearchService<HitsService.Request, ResponseSearch> {
 
-    override suspend fun search(request: IndexQuery, requestOptions: RequestOptions?): ResponseSearch {
-        // TODO: disjunctive faceting
-        val index = client.initIndex(request.indexName)
-        return index.search(request.query, requestOptions)
+    private val searchService = AdvancedSearchService(client)
+
+    override suspend fun search(request: Request, requestOptions: RequestOptions?): ResponseSearch {
+        val (query, filterGroups) = request
+        return when {
+            filterGroups != null -> searchService.search(query, filterGroups, requestOptions)
+            else -> indexSearch(request.indexQuery, requestOptions)
+        }
     }
 
-    // Other approach:
-    // val queries = listOf(request)
-    // val response = client.search(requests = queries, requestOptions = requestOptions)
-    // return response.results.first().asResponseSearch()
-    //
-    // private fun ResultMultiSearch<ResultSearch>.asResponseSearch() = (this as ResultMultiSearch.Hits).response
+    private suspend fun indexSearch(indexQuery: IndexQuery, requestOptions: RequestOptions? = null): ResponseSearch {
+        val index = client.initIndex(indexQuery.indexName)
+        return index.search(indexQuery.query, requestOptions)
+    }
+
+    data class Request(
+        val indexQuery: IndexQuery,
+        val filterGroups: Set<FilterGroup<*>>? = null
+    )
 }
