36a9e14c3984e2404a715bf6dc531293294ab184
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-10-27 15:45:18 +0200

    add filterstate connection to hits searcher

---
+++ instantsearch/src/commonMain/kotlin/com/algolia/instantsearch/helper/searcher/internal/HitsSearcherConnectionFilterState.kt
@@ -0,0 +1,44 @@
+package com.algolia.instantsearch.helper.searcher.internal
+
+import com.algolia.instantsearch.core.Callback
+import com.algolia.instantsearch.core.ExperimentalInstantSearch
+import com.algolia.instantsearch.core.connection.ConnectionImpl
+import com.algolia.instantsearch.core.searcher.Debouncer
+import com.algolia.instantsearch.helper.filter.state.FilterState
+import com.algolia.instantsearch.helper.filter.state.Filters
+import com.algolia.instantsearch.helper.filter.state.toFilterGroups
+import com.algolia.instantsearch.helper.searcher.SearcherSingleIndex
+import com.algolia.instantsearch.helper.searcher.hits.HitsSearcher
+import com.algolia.search.model.filter.FilterGroupsConverter
+
+@ExperimentalInstantSearch
+internal data class HitsSearcherConnectionFilterState(
+    private val searcher: HitsSearcher,
+    private val filterState: FilterState,
+    private val debouncer: Debouncer,
+) : ConnectionImpl() {
+
+    private val updateSearcher: Callback<Filters> = { filters ->
+        searcher.updateFilters(filters)
+        debouncer.debounce(searcher) { searchAsync() }
+    }
+
+    init {
+        searcher.updateFilters()
+    }
+
+    override fun connect() {
+        super.connect()
+        filterState.filters.subscribe(updateSearcher)
+    }
+
+    override fun disconnect() {
+        super.disconnect()
+        filterState.filters.unsubscribe(updateSearcher)
+    }
+
+    private fun HitsSearcher.updateFilters(filters: Filters = filterState) {
+        filterGroups = filters.toFilterGroups()
+        query.filters = FilterGroupsConverter.SQL(filterGroups)
+    }
+}
