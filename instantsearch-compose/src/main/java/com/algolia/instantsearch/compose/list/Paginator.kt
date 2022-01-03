f730242030351dfa8566c0698fa325dadfeb0a7e
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-01-03 11:15:21 +0100

    remove paginator experimental/deprecated function factory

--- instantsearch-compose/src/main/java/com/algolia/instantsearch/compose/list/Paginator.kt
+++ instantsearch-compose/src/main/java/com/algolia/instantsearch/compose/list/Paginator.kt
@@ -5,13 +5,9 @@ package com.algolia.instantsearch.compose.list
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.algolia.instantsearch.ExperimentalInstantSearch
-import com.algolia.instantsearch.compose.list.internal.SearcherMultipleIndexPagingSource
 import com.algolia.instantsearch.compose.list.internal.SearcherPaginator
 import com.algolia.instantsearch.compose.list.internal.SearcherPagingSource
-import com.algolia.instantsearch.helper.searcher.SearcherMultipleIndex
-import com.algolia.instantsearch.helper.searcher.SearcherSingleIndex
 import com.algolia.instantsearch.helper.searcher.util.SearcherForHits
-import com.algolia.search.model.multipleindex.IndexQuery
 import com.algolia.search.model.params.SearchParameters
 import com.algolia.search.model.response.ResponseSearch
 import kotlinx.coroutines.flow.Flow
@@ -34,7 +30,7 @@ public interface Paginator<T : Any> {
 }
 
 /**
- * Paginator for [SearcherSingleIndex].
+ * Paginator for [SearcherForHits].
  *
  * @param searcher single index searcher
  * @param pagingConfig configure loading behavior within a Pager
@@ -48,23 +44,4 @@ public fun <T : Any> Paginator(
 ): Paginator<T> = SearcherPaginator(
     pagingConfig = pagingConfig,
     pagingSourceFactory = { SearcherPagingSource(searcher, transformer) }
-)
-
-/**
- * Paginator for [SearcherMultipleIndex].
- *
- * @param searcher multiple index searcher
- * @param indexQuery query associated to a specific index
- * @param pagingConfig configure loading behavior within a Pager
- * @param transformer mapping applied to search responses
- */
-@ExperimentalInstantSearch
-public fun <T : Any> Paginator(
-    searcher: SearcherMultipleIndex,
-    indexQuery: IndexQuery,
-    pagingConfig: PagingConfig = PagingConfig(pageSize = 10),
-    transformer: (ResponseSearch.Hit) -> T
-): Paginator<T> = SearcherPaginator(
-    pagingConfig = pagingConfig,
-    pagingSourceFactory = { SearcherMultipleIndexPagingSource(searcher, indexQuery, transformer) }
 )
