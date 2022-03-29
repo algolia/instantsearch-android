e53327d9850e430fefb58aebaf9bc0256366f729
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-03-29 09:01:39 +0200

    apply lint

--- instantsearch-insights/src/commonMain/kotlin/com/algolia/instantsearch/insights/internal/uploader/InsightsUploader.kt
+++ instantsearch-insights/src/commonMain/kotlin/com/algolia/instantsearch/insights/internal/uploader/InsightsUploader.kt
@@ -3,7 +3,7 @@ package com.algolia.instantsearch.insights.internal.uploader
 import com.algolia.instantsearch.insights.internal.event.EventResponse
 import com.algolia.search.configuration.Credentials
 
-internal interface InsightsUploader: Credentials {
+internal interface InsightsUploader : Credentials {
 
     suspend fun uploadAll(): List<EventResponse>
 }
