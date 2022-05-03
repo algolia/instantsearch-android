b35734a5bb1f6cc2483b54aa1d071c8dc91f4c7c
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-05-03 14:42:58 +0200

    remove query suggestion guide

--- examples/src/main/kotlin/com/algolia/instantsearch/examples/directory/Directory.kt
+++ examples/src/main/kotlin/com/algolia/instantsearch/examples/directory/Directory.kt
@@ -10,14 +10,12 @@ import com.algolia.instantsearch.examples.codex.voice.MainActivity as VoiceSearc
 import com.algolia.instantsearch.examples.guides.compose.ComposeActivity
 import com.algolia.instantsearch.examples.guides.gettingstarted.GettingStartedGuide
 import com.algolia.instantsearch.examples.guides.insights.InsightsActivity
-import com.algolia.instantsearch.examples.guides.querysuggestion.QuerySuggestionActivity
 import com.algolia.instantsearch.examples.showcase.compose.directory.ComposeDirectoryShowcase
 import com.algolia.instantsearch.examples.showcase.view.directory.AndroidViewDirectoryShowcase
 import com.algolia.search.model.ObjectID
 
 val guides = mapOf(
     ObjectID("guide_getting_started") to GettingStartedGuide::class,
-    ObjectID("guide_query_suggestion") to QuerySuggestionActivity::class,
     ObjectID("guide_insights") to InsightsActivity::class,
     ObjectID("guide_declarative_ui") to ComposeActivity::class,
     ObjectID("showcase_imperative_ui") to AndroidViewDirectoryShowcase::class,
