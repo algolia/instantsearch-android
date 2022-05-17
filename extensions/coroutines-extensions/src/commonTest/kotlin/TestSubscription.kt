e5b28757b62b03720291239278ee2808914f163c
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-05-17 16:50:01 +0200

    remove unstable subscription value extension

--- extensions/coroutines-extensions/src/commonTest/kotlin/TestSubscription.kt
+++ extensions/coroutines-extensions/src/commonTest/kotlin/TestSubscription.kt
@@ -3,16 +3,10 @@ import com.algolia.instantsearch.ExperimentalInstantSearch
 import com.algolia.instantsearch.core.subscription.SubscriptionEvent
 import com.algolia.instantsearch.core.subscription.SubscriptionValue
 import com.algolia.instantsearch.coroutines.asFlow
-import com.algolia.instantsearch.coroutines.stateIn
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancelAndJoin
-import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlinx.coroutines.isActive
-
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 
 @OptIn(ExperimentalInstantSearch::class, ExperimentalCoroutinesApi::class)
 class TestSubscription {
@@ -45,29 +39,5 @@ class TestSubscription {
             // wrap up
             cancel()
         }
-    }
-
-    @Test
-    fun testSubscriptionValueToStateFlow() = runTest {
-        val subscriptionEvent = SubscriptionValue<String?>("event0")
-        val job = Job()
-        val scope = CoroutineScope(job)
-        val flow = subscriptionEvent.stateIn(scope)
-        flow.test {
-            assertEquals("event0", awaitItem())
-            // send : "event1"
-            subscriptionEvent.value = "event1"
-            assertEquals("event1", awaitItem())
-            assertEquals("event1", flow.value)
-            // send : "event2"
-            subscriptionEvent.value = "event2"
-            assertEquals("event2", awaitItem())
-            assertEquals("event2", flow.value)
-            // wrap up
-            cancel()
-        }
-        job.cancelAndJoin()
-        subscriptionEvent.value = "event3"
-        assertEquals("event2", flow.value)
     }
 }
