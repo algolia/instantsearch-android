007c8999764afb6a15afbfc0bad94669308b3b28
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-06-02 16:40:24 +0200

    init test

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -12,14 +12,24 @@ android {
 
         testInstrumentationRunner = "androidx.benchmark.junit4.AndroidBenchmarkRunner"
     }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.1"
+    }
 }
 
 dependencies {
-    implementation(project(":instantsearch"))
+    implementation(project(":instantsearch-compose"))
 
-    androidTestImplementation("androidx.activity:activity-ktx:1.4.0")
+    androidTestImplementation("androidx.benchmark:benchmark-junit4:1.0.0")
+
+    androidTestImplementation("androidx.activity:activity-compose:1.4.0")
+    androidTestImplementation("androidx.compose.ui:ui:1.1.1")
+    androidTestImplementation("androidx.compose.material:material:1.1.1")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test:rules:1.4.0")
-    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.2.0")
-    androidTestImplementation("androidx.benchmark:benchmark-junit4:1.0.0")
 }
--- benchmark/src/androidTest/kotlin/com/algolia/instantsearch/benchmark/SearcherBenchmark.kt
+++ benchmark/src/androidTest/kotlin/com/algolia/instantsearch/benchmark/SearcherBenchmark.kt
@@ -1,42 +1,33 @@
 package com.algolia.instantsearch.benchmark
 
-import androidx.activity.ComponentActivity
-import androidx.benchmark.junit4.BenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.algolia.instantsearch.benchmark.utils.AndroidBenchmarkRule
+import com.algolia.instantsearch.searcher.hits.HitsSearcher
+import com.algolia.search.model.APIKey
+import com.algolia.search.model.ApplicationID
+import com.algolia.search.model.IndexName
 import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.RuleChain
-import org.junit.rules.TestRule
-import org.junit.runner.Description
 import org.junit.runner.RunWith
-import org.junit.runners.model.Statement
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class SearcherBenchmark {
 
     @get:Rule
-    val rule = SimpleAndroidBenchmarkRule()
+    val rule = AndroidBenchmarkRule()
 
     @Test
-    fun startup() {
+    fun searcher() {
+        val searcher = HitsSearcher(
+            applicationID = ApplicationID("latency"),
+            apiKey = APIKey("afc3dd66dd1293e2e2736a5a51b05c0a"),
+            indexName = IndexName("instant_search")
+        )
+
         rule.activityTestRule.runOnUiThread {
-            val i = 1 + 1
-            println(i)
-        }
-    }
 
-    class SimpleAndroidBenchmarkRule() : TestRule {
-        @Suppress("DEPRECATION")
-        val activityTestRule = androidx.test.rule.ActivityTestRule(ComponentActivity::class.java)
-
-        val benchmarkRule = BenchmarkRule()
-
-        override fun apply(base: Statement, description: Description): Statement {
-            return RuleChain.outerRule(benchmarkRule)
-                .around(activityTestRule)
-                .apply(base, description)
         }
     }
 }
