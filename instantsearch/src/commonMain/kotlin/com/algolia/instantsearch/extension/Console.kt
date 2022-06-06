968467dfae826e5091cadd1122c9ed223673817d
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-06-06 17:15:09 +0200

    add throwable default value

--- instantsearch/src/commonMain/kotlin/com/algolia/instantsearch/extension/Console.kt
+++ instantsearch/src/commonMain/kotlin/com/algolia/instantsearch/extension/Console.kt
@@ -2,11 +2,11 @@ package com.algolia.instantsearch.extension
 
 internal expect object Console {
 
-    fun debug(message: String, throwable: Throwable?)
+    fun debug(message: String, throwable: Throwable? = null)
 
-    fun info(message: String, throwable: Throwable?)
+    fun info(message: String, throwable: Throwable? = null)
 
-    fun warn(message: String, throwable: Throwable?)
+    fun warn(message: String, throwable: Throwable? = null)
 
-    fun error(message: String, throwable: Throwable?)
+    fun error(message: String, throwable: Throwable? = null)
 }
