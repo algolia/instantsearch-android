0d1a638849a956d07f3ffdb13c947a4ad638c12c
Dhaya <154633+dhayab@users.noreply.github.com>
2025-07-29 16:27:14 +0200

    fix(paging): force initial load size to match page size (#411)
    * fix(paging): force initial load size to match page size
    
    * fix ci

--- extensions/android-paging3/src/main/kotlin/com/algolia/instantsearch/android/paging3/Paginator.kt
+++ extensions/android-paging3/src/main/kotlin/com/algolia/instantsearch/android/paging3/Paginator.kt
@@ -9,6 +9,7 @@ import androidx.paging.PagingData
 import androidx.paging.liveData
 import com.algolia.instantsearch.android.paging3.internal.SearcherPaginator
 import com.algolia.instantsearch.android.paging3.internal.SearcherPagingSource
+import com.algolia.instantsearch.extension.Console
 import com.algolia.instantsearch.searcher.SearcherForHits
 import com.algolia.search.model.params.SearchParameters
 import com.algolia.search.model.response.ResponseSearch
@@ -41,10 +42,25 @@ public fun <T : Any> Paginator(
     searcher: SearcherForHits<out SearchParameters>,
     pagingConfig: PagingConfig = PagingConfig(pageSize = 10, initialLoadSize = 10),
     transformer: (ResponseSearch.Hit) -> T
-): Paginator<T> = SearcherPaginator(
-    pagingConfig = pagingConfig,
-    pagingSourceFactory = { SearcherPagingSource(searcher, transformer) }
-)
+): Paginator<T> {
+    if (pagingConfig.initialLoadSize != pagingConfig.pageSize) {
+        Console.warn("Overriding initialLoadSize (${pagingConfig.initialLoadSize}) to match pageSize (${pagingConfig.pageSize})")
+    }
+
+    val validPagingConfig = PagingConfig(
+        pageSize = pagingConfig.pageSize,
+        initialLoadSize = pagingConfig.pageSize,
+        enablePlaceholders = pagingConfig.enablePlaceholders,
+        prefetchDistance = pagingConfig.prefetchDistance,
+        jumpThreshold = pagingConfig.jumpThreshold,
+        maxSize = pagingConfig.maxSize
+    )
+
+    return SearcherPaginator(
+        pagingConfig = validPagingConfig,
+        pagingSourceFactory = { SearcherPagingSource(searcher, transformer) }
+    )
+}
 
 /**
  * A cold Flow of [PagingData], emits new instances of [PagingData] once they become invalidated.
