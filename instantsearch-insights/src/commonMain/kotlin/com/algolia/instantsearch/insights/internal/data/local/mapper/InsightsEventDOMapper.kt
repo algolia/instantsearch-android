e84f1d0dcda7349951d87cbe1f89d81d1481fd2a
Andras <ferencz.andras@192.168.0.160>
2026-01-29 12:43:20 +0200

    Code Review Update

--- instantsearch-insights/src/commonMain/kotlin/com/algolia/instantsearch/insights/internal/data/local/mapper/InsightsEventDOMapper.kt
+++ instantsearch-insights/src/commonMain/kotlin/com/algolia/instantsearch/insights/internal/data/local/mapper/InsightsEventDOMapper.kt
@@ -6,11 +6,11 @@ import com.algolia.instantsearch.insights.internal.extension.JsonNonStrict
 
 internal object InsightsEventDOMapper : Mapper<InsightsEventDO, String> {
 
-    override fun map(input: com.algolia.instantsearch.insights.internal.data.local.model.InsightsEventDO): String {
+    override fun map(input: InsightsEventDO): String {
         return JsonNonStrict.encodeToString(InsightsEventDO.serializer(), input)
     }
 
-    override fun unmap(input: String): com.algolia.instantsearch.insights.internal.data.local.model.InsightsEventDO {
+    override fun unmap(input: String): InsightsEventDO {
         return JsonNonStrict.decodeFromString(InsightsEventDO.serializer(), input)
     }
 }
