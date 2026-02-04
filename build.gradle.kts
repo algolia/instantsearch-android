82cb0cc9c689e2f2d4e09fa340634822ecd27c82
Andras <ferencz.andras@192.168.0.116>
2026-02-04 15:33:38 +0200

    (chore): Upgrade dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.spotless) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.dokka) apply false
 }
 
 subprojects {
